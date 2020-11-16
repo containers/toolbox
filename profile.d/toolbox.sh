containers/toolbox

commit c6e37cdef37e2276207b34a42919f1e0ac4a04dc

profile.d: Tighten the Silverblue check
The Silverblue welcome message was being displayed incorrectly on
other OSTree based OS's (Fedora Atomic Host, Fedora CoreOS, etc).

Note that none of the stable Silverblue releases that have shipped so
far (ie., until Silverblue 30) have had 'silverblue' as the VARIANT_ID.
This makes the check a bit more convoluted that it should have been.

https://github.com/debarshiray/toolbox/pull/236

profile.d/toolbox.sh
@@ -5,8 +5,13 @@ toolbox_config="$HOME/.config/toolbox"
 host_welcome_stub="$toolbox_config/host-welcome-shown"
 toolbox_welcome_stub="$toolbox_config/toolbox-welcome-shown"
 
+# shellcheck disable=SC1091
+. /usr/lib/os-release
+
 if [ -f /run/ostree-booted ] \
-   && ! [ -f "$host_welcome_stub" ]; then
+   && ! [ -f "$host_welcome_stub" ] \
+   && [ "${ID}" = "fedora" ] \
+   && { [ "${VARIANT_ID}" = "workstation" ] || [ "${VARIANT_ID}" = "silverblue" ]; }; then
     echo ""
     echo "Welcome to Fedora Silverblue. This terminal is running on the"
     echo "host system. You may want to try out the Toolbox for a directly"

commit dcdfa3a1f5a3421330397ea72c0df132a260853d

profile.d: Don't leak all the os-release fields into the shell
All the fields defined in /usr/lib/os-release were being injected into
the shell as environment variables. This is unintentional. Some of the
variables have relatively generic names, and having them in the
environment can lead to unexpected surprises.

Fallout from c6e37cdef37e2276207b34a42919f1e0ac4a04dc

https://github.com/containers/toolbox/pull/623

profile.d/toolbox.sh
@@ -5,8 +5,14 @@ toolbox_config="$HOME/.config/toolbox"
 host_welcome_stub="$toolbox_config/host-welcome-shown"
 toolbox_welcome_stub="$toolbox_config/toolbox-welcome-shown"
 
-# shellcheck disable=SC1091
-. /usr/lib/os-release
+# shellcheck disable=2046
+eval $(
+          # shellcheck disable=SC1091
+          . /usr/lib/os-release
+
+          echo ID="$ID"
+          echo VARIANT_ID="$VARIANT_ID"
+      )
 
 if [ -f /run/ostree-booted ] \
    && ! [ -f "$host_welcome_stub" ] \