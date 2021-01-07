containers/toolbox

commit dcdfa3a1f5a3421330397ea72c0df132a260853d

profile.d: Don't leak all the os-release fields into the shell
All the fields defined in /usr/lib/os-release were being injected into
the shell as environment variables. This is unintentional. Some of the
variables have relatively generic names, and having them in the
environment can lead to unexpected surprises.

Fallout from c6e37cdef37e2276207b34a42919f1e0ac4a04dc

https://github.com/containers/toolbox/pull/623

profile.d/toolbox.sh
@@ -5,8 +5,14 @@ toolbox_config="$HOME/.config/toolbox"
 host_welcome_stub="$toolbox_config/host-welcome-shown"
 toolbox_welcome_stub="$toolbox_config/toolbox-welcome-shown"
 
-# shellcheck disable=SC1091
-. /usr/lib/os-release
+# shellcheck disable=2046
+eval $(
+          # shellcheck disable=SC1091
+          . /usr/lib/os-release
+
+          echo ID="$ID"
+          echo VARIANT_ID="$VARIANT_ID"
+      )
 
 if [ -f /run/ostree-booted ] \
    && ! [ -f "$host_welcome_stub" ] \

commit 88f2916822f1fdf71caff9d376587adc86b57f20

profile.d: Restore compatibility with Z shell
Otherwise, every zsh instance was running into:
  zsh: #: command not found...

Fallout from dcdfa3a1f5a3421330397ea72c0df132a260853d

https://github.com/containers/toolbox/pull/660

profile.d/toolbox.sh
@@ -6,8 +6,8 @@ host_welcome_stub="$toolbox_config/host-welcome-shown"
 toolbox_welcome_stub="$toolbox_config/toolbox-welcome-shown"
 
 # shellcheck disable=2046
+# shellcheck disable=SC1091
 eval $(
-          # shellcheck disable=SC1091
           . /usr/lib/os-release
 
           echo ID="$ID"