containers/toolbox

commit 06dcdbe2a6e057f28b1cf02a8193db77dfa2f4f2

cmd/rm,rmi: Use function for getting list of images/containers
Instead of using own ways of getting all toolbox images/containers (and
thus unnecessary duplication of code) use a dedicated function.

src/cmd/rm.go
@@ -24,7 +24,6 @@ import (
 
 	"github.com/containers/toolbox/pkg/podman"
 	"github.com/containers/toolbox/pkg/utils"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -70,31 +69,15 @@ func rm(cmd *cobra.Command, args []string) error {
 	}
 
 	if rmFlags.deleteAll {
-		logrus.Debug("Fetching containers with label=com.github.containers.toolbox=true")
-		args := []string{"--all", "--filter", "label=com.github.containers.toolbox=true"}
-		containers_old, err := podman.GetContainers(args...)
+		var toolboxContainers []toolboxContainer
+
+		toolboxContainers, err := listContainers()
 		if err != nil {
-			return fmt.Errorf("failed to list containers with label=com.github.containers.toolbox=true: %w", err)
+			return err
 		}
 
-		logrus.Debug("Fetching containers with label=com.github.debarshiray.toolbox=true")
-		args = []string{"--all", "--filter", "label=com.github.debarshiray.toolbox=true"}
-		containers_new, err := podman.GetContainers(args...)
-		if err != nil {
-			return fmt.Errorf("failed to list containers with com.github.debarshiray.toolbox=true: %w", err)
-		}
-
-		var idKey string
-		if podman.CheckVersion("2.0.0") {
-			idKey = "Id"
-		} else {
-			idKey = "ID"
-		}
-
-		containers := utils.JoinJSON(idKey, containers_old, containers_new)
-
-		for _, container := range containers {
-			containerID := container[idKey].(string)
+		for _, container := range toolboxContainers {
+			containerID := container.ID
 			if err := podman.RemoveContainer(containerID, rmFlags.forceDelete); err != nil {
 				fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 				continue

src/cmd/rmi.go
@@ -24,7 +24,6 @@ import (
 
 	"github.com/containers/toolbox/pkg/podman"
 	"github.com/containers/toolbox/pkg/utils"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -70,33 +69,15 @@ func rmi(cmd *cobra.Command, args []string) error {
 	}
 
 	if rmiFlags.deleteAll {
-		logrus.Debug("Fetching images with label=com.github.containers.toolbox=true")
-		args := []string{"--filter", "label=com.github.containers.toolbox=true"}
-		images_old, err := podman.GetImages(args...)
+		var toolboxImages []toolboxImage
+
+		toolboxImages, err := listImages()
 		if err != nil {
-			return fmt.Errorf("failed to list images with label=com.github.containers.toolbox=true: %w", err)
+			return err
 		}
 
-		logrus.Debug("Fetching images with label=com.github.debarshiray.toolbox=true")
-		args = []string{"--filter", "label=com.github.debarshiray.toolbox=true"}
-		images_new, err := podman.GetImages(args...)
-		if err != nil {
-			return fmt.Errorf("failed to list images with com.github.debarshiray.toolbox=true: %w", err)
-		}
-
-		var idKey string
-		if podman.CheckVersion("2.0.0") {
-			idKey = "Id"
-		} else if podman.CheckVersion("1.8.3") {
-			idKey = "ID"
-		} else {
-			idKey = "id"
-		}
-
-		images := utils.JoinJSON(idKey, images_old, images_new)
-
-		for _, image := range images {
-			imageID := image[idKey].(string)
+		for _, image := range toolboxImages {
+			imageID := image.ID
 			if err := podman.RemoveImage(imageID, rmiFlags.forceDelete); err != nil {
 				fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 				continue

commit 4fda534d05d2f057f70545e4324ae29b172e393e

cmd/rm,rmi: Properly rename called function
Fallout from: 06dcdbe2a6e057f28b1cf02a8193db77dfa2f4f2

https://github.com/containers/toolbox/pull/738

src/cmd/rm.go
@@ -71,7 +71,7 @@ func rm(cmd *cobra.Command, args []string) error {
 	if rmFlags.deleteAll {
 		var toolboxContainers []toolboxContainer
 
-		toolboxContainers, err := listContainers()
+		toolboxContainers, err := getContainers()
 		if err != nil {
 			return err
 		}

src/cmd/rmi.go
@@ -71,7 +71,7 @@ func rmi(cmd *cobra.Command, args []string) error {
 	if rmiFlags.deleteAll {
 		var toolboxImages []toolboxImage
 
-		toolboxImages, err := listImages()
+		toolboxImages, err := getImages()
 		if err != nil {
 			return err
 		}