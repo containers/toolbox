d14a94c5fc23bf146980de52caafd1cf66b3241e
Ondřej Míchal <harrymichal@seznam.cz>
2021-07-04 00:16:29 +0200

    pkg/utils: Drop redundant functions
    https://github.com/containers/toolbox/pull/833

--- src/pkg/utils/utils.go
+++ src/pkg/utils/utils.go
@@ -25,7 +25,6 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
-	"sort"
 	"strconv"
 	"strings"
 	"syscall"
@@ -673,28 +672,6 @@ func IsInsideToolboxContainer() bool {
 	return false
 }
 
-func JoinJSON(joinkey string, maps ...[]map[string]interface{}) []map[string]interface{} {
-	var json []map[string]interface{}
-	found := make(map[string]bool)
-
-	// Iterate over every json provided and check if it is already in the final json
-	// If it contains some invalid entry (equals nil), then skip that entry
-
-	for _, m := range maps {
-		for _, entry := range m {
-			if entry["names"] == nil && entry["Names"] == nil {
-				continue
-			}
-			key := entry[joinkey].(string)
-			if _, ok := found[key]; !ok {
-				found[key] = true
-				json = append(json, entry)
-			}
-		}
-	}
-	return json
-}
-
 // ResolveContainerAndImageNames takes care of standardizing names of containers and images.
 //
 // If no image name is specified then the base image will reflect the platform of the host (even the version).
@@ -776,15 +753,4 @@ func ShowManual(manual string) error {
 	}
 
 	return nil
-}
-
-func SortJSON(json []map[string]interface{}, key string, hasInterface bool) []map[string]interface{} {
-	sort.Slice(json, func(i, j int) bool {
-		if hasInterface {
-			return json[i][key].([]interface{})[0].(string) < json[j][key].([]interface{})[0].(string)
-		}
-		return json[i][key].(string) < json[j][key].(string)
-	})
-
-	return json
 }
