containers/toolbox

commit dd947016b3a9098f23b2a77a3616a4f5891ae8ab

pkg/utils: Add function to check if an image is referenced by its ID
https://github.com/containers/toolbox/pull/318

src/pkg/utils/utils.go
@@ -271,6 +271,12 @@ func GetMountOptions(target string) (string, error) {
 	return mountOptions, nil
 }
 
+// ImageReferenceCanBeID checks if 'image' might be the ID of an image
+func ImageReferenceCanBeID(image string) (bool, error) {
+	matched, err := regexp.MatchString("^[a-f0-9]\\{6,64\\}$", image)
+	return matched, err
+}
+
 // ShortID shortens provided id to first 12 characters.
 func ShortID(id string) string {
 	if len(id) > idTruncLength {

commit 781c433a8db4460629b2e1568d8cbe17b0af0b2f

pkg/utils: Fix regular expression used to find IDs in image references
Turns out the braces do not need to be escaped.

The equivalent code in the POSIX shell implementation was:
  echo "$image" | grep "^[a-f0-9]\{6,64\}$"

There the braces had to be escaped because it was using grep(1) with
basic regular expressions (ie., without the --extended-regexp flag),
where the meta-characters ?, +, {, |, ( and ) lose their special
meaning unless they are escaped.

However, that was grep(1), and this is Go's regexp package.

Fallout from dd947016b3a9098f23b2a77a3616a4f5891ae8ab

https://github.com/containers/toolbox/pull/825

src/pkg/utils/utils.go
@@ -501,7 +501,7 @@ func HumanDuration(duration int64) string {
 
 // ImageReferenceCanBeID checks if 'image' might be the ID of an image
 func ImageReferenceCanBeID(image string) bool {
-	matched, err := regexp.MatchString("^[a-f0-9]\\{6,64\\}$", image)
+	matched, err := regexp.MatchString("^[a-f0-9]{6,64}$", image)
 	if err != nil {
 		panic("regular expression for ID reference matching is invalid")
 	}