containers/toolbox

commit c990fb43cad5f1704584f119ba4f57074c1d0bc9

Introduce the concept of a 'distro' for future non-Fedora containers
This shouldn't have any user-visible effect. However, it parametrizes
various code paths that were currently hard-coded to represent Fedora.
Subsequent commits will build upon this to support toolbox containers
of operating system distributions that aren't Fedora.

A map of supported operating system distributions has been introduced
that lists out the necessary bits of metadata needed to work with them.
In the future, it should just be a matter of adding new entries to this
map to add support for distributions that aren't Fedora.

https://github.com/containers/toolbox/pull/667

src/pkg/utils/utils.go
@@ -39,20 +39,31 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+type ParseReleaseFunc func(string) (string, error)
+
+type Distro struct {
+	ContainerNamePrefix    string
+	ImageBasename          string
+	ParseRelease           ParseReleaseFunc
+	Registry               string
+	Repository             string
+	RepositoryNeedsRelease bool
+}
+
 const (
 	idTruncLength          = 12
 	releaseDefaultFallback = "32"
 )
 
 const (
-	ContainerNamePrefixDefault = "fedora-toolbox"
-
 	// Based on the nameRegex value in:
 	// https://github.com/containers/libpod/blob/master/libpod/options.go
 	ContainerNameRegexp = "[a-zA-Z0-9][a-zA-Z0-9_.-]*"
 )
 
 var (
+	distroDefault = "fedora"
+
 	preservedEnvironmentVariables = []string{
 		"COLORTERM",
 		"DBUS_SESSION_BUS_ADDRESS",
@@ -80,10 +91,22 @@ var (
 	}
 
 	releaseDefault string
+
+	supportedDistros = map[string]Distro{
+		"fedora": {
+			"fedora-toolbox",
+			"fedora-toolbox",
+			parseReleaseFedora,
+			"registry.fedoraproject.org",
+			"f%s",
+			true,
+		},
+	}
 )
 
 var (
-	ContainerNameDefault string
+	ContainerNameDefault       string
+	ContainerNamePrefixDefault = "fedora-toolbox"
 )
 
 func init() {
@@ -91,9 +114,11 @@ func init() {
 
 	hostID, err := GetHostID()
 	if err == nil {
-		if hostID == "fedora" {
+		if distroObj, supportedDistro := supportedDistros[hostID]; supportedDistro {
 			release, err := GetHostVersionID()
 			if err == nil {
+				ContainerNamePrefixDefault = distroObj.ContainerNamePrefix
+				distroDefault = hostID
 				releaseDefault = release
 			}
 		}
@@ -236,6 +261,38 @@ func GetCgroupsVersion() (int, error) {
 	return version, nil
 }
 
+func GetContainerNamePrefixForImage(image string) (string, error) {
+	basename := ImageReferenceGetBasename(image)
+	if basename == "" {
+		return "", fmt.Errorf("failed to get the basename of image %s", image)
+	}
+
+	for _, distroObj := range supportedDistros {
+		if distroObj.ImageBasename != basename {
+			continue
+		}
+
+		return distroObj.ContainerNamePrefix, nil
+	}
+
+	return basename, nil
+}
+
+func GetDefaultImageForDistro(distro, release string) string {
+	if _, supportedDistro := supportedDistros[distro]; !supportedDistro {
+		distro = "fedora"
+	}
+
+	distroObj, supportedDistro := supportedDistros[distro]
+	if !supportedDistro {
+		panicMsg := fmt.Sprintf("failed to find %s in the list of supported distributions", distro)
+		panic(panicMsg)
+	}
+
+	image := distroObj.ImageBasename + ":" + release
+	return image
+}
+
 func GetEnvOptionsForPreservedVariables() []string {
 	logrus.Debug("Creating list of environment variables to forward")
 
@@ -253,6 +310,41 @@ func GetEnvOptionsForPreservedVariables() []string {
 	}
 
 	return envOptions
+}
+
+func GetFullyQualifiedImageFromDistros(image, release string) (string, error) {
+	logrus.Debugf("Resolving fully qualified name for image %s from known registries", image)
+
+	if ImageReferenceHasDomain(image) {
+		return image, nil
+	}
+
+	basename := ImageReferenceGetBasename(image)
+	if basename == "" {
+		return "", fmt.Errorf("failed to get the basename of image %s", image)
+	}
+
+	for _, distroObj := range supportedDistros {
+		if distroObj.ImageBasename != basename {
+			continue
+		}
+
+		var repository string
+
+		if distroObj.RepositoryNeedsRelease {
+			repository = fmt.Sprintf(distroObj.Repository, release)
+		} else {
+			repository = distroObj.Repository
+		}
+
+		imageFull := distroObj.Registry + "/" + repository + "/" + image
+
+		logrus.Debugf("Resolved image %s to %s", image, imageFull)
+
+		return imageFull, nil
+	}
+
+	return "", fmt.Errorf("failed to resolve image %s")
 }
 
 // GetGroupForSudo returns the name of the sudoers group.
@@ -467,7 +559,27 @@ func ShortID(id string) string {
 	return id
 }
 
-func ParseRelease(str string) (string, error) {
+func ParseRelease(distro, str string) (string, error) {
+	if distro == "" {
+		distro = distroDefault
+	}
+
+	if _, supportedDistro := supportedDistros[distro]; !supportedDistro {
+		distro = "fedora"
+	}
+
+	distroObj, supportedDistro := supportedDistros[distro]
+	if !supportedDistro {
+		panicMsg := fmt.Sprintf("failed to find %s in the list of supported distributions", distro)
+		panic(panicMsg)
+	}
+
+	parseRelease := distroObj.ParseRelease
+	release, err := parseRelease(str)
+	return release, err
+}
+
+func parseReleaseFedora(str string) (string, error) {
 	var release string
 
 	if strings.HasPrefix(str, "F") || strings.HasPrefix(str, "f") {
@@ -553,18 +665,23 @@ func JoinJSON(joinkey string, maps ...[]map[string]interface{}) []map[string]int
 // If no container name is specified then the name of the image will be used.
 //
 // If the host system is unknown then the base image will be 'fedora-toolbox' with a default version
-func ResolveContainerAndImageNames(container, image, release string) (string, string, string, error) {
+func ResolveContainerAndImageNames(container, distro, image, release string) (string, string, string, error) {
 	logrus.Debug("Resolving container and image names")
 	logrus.Debugf("Container: '%s'", container)
+	logrus.Debugf("Distribution: '%s'", distro)
 	logrus.Debugf("Image: '%s'", image)
 	logrus.Debugf("Release: '%s'", release)
+
+	if distro == "" {
+		distro = distroDefault
+	}
 
 	if release == "" {
 		release = releaseDefault
 	}
 
 	if image == "" {
-		image = "fedora-toolbox:" + release
+		image = GetDefaultImageForDistro(distro, release)
 	} else {
 		release = ImageReferenceGetTag(image)
 		if release == "" {
@@ -573,12 +690,11 @@ func ResolveContainerAndImageNames(container, image, release string) (string, st
 	}
 
 	if container == "" {
-		basename := ImageReferenceGetBasename(image)
-		if basename == "" {
-			return "", "", "", fmt.Errorf("failed to get the basename of image %s", image)
+		var err error
+		container, err = GetContainerNamePrefixForImage(image)
+		if err != nil {
+			return "", "", "", err
 		}
-
-		container = basename
 
 		tag := ImageReferenceGetTag(image)
 		if tag != "" {

src/cmd/create.go
@@ -130,7 +130,7 @@ func create(cmd *cobra.Command, args []string) error {
 	var release string
 	if createFlags.release != "" {
 		var err error
-		release, err = utils.ParseRelease(createFlags.release)
+		release, err = utils.ParseRelease("", createFlags.release)
 		if err != nil {
 			err := utils.CreateErrorInvalidRelease(executableBase)
 			return err
@@ -138,6 +138,7 @@ func create(cmd *cobra.Command, args []string) error {
 	}
 
 	container, image, release, err := utils.ResolveContainerAndImageNames(container,
+		"",
 		createFlags.image,
 		release)
 	if err != nil {
@@ -659,7 +660,11 @@ func pullImage(image, release string) (bool, error) {
 	if hasDomain {
 		imageFull = image
 	} else {
-		imageFull = fmt.Sprintf("registry.fedoraproject.org/f%s/%s", release, image)
+		var err error
+		imageFull, err = utils.GetFullyQualifiedImageFromDistros(image, release)
+		if err != nil {
+			return false, fmt.Errorf("image %s not found in local storage and known registries", image)
+		}
 	}
 
 	logrus.Debugf("Looking for image %s", imageFull)

src/cmd/enter.go
@@ -102,14 +102,14 @@ func enter(cmd *cobra.Command, args []string) error {
 		nonDefaultContainer = true
 
 		var err error
-		release, err = utils.ParseRelease(enterFlags.release)
+		release, err = utils.ParseRelease("", enterFlags.release)
 		if err != nil {
 			err := utils.CreateErrorInvalidRelease(executableBase)
 			return err
 		}
 	}
 
-	container, image, release, err := utils.ResolveContainerAndImageNames(container, "", release)
+	container, image, release, err := utils.ResolveContainerAndImageNames(container, "", "", release)
 	if err != nil {
 		return err
 	}

src/cmd/run.go
@@ -97,7 +97,7 @@ func run(cmd *cobra.Command, args []string) error {
 		nonDefaultContainer = true
 
 		var err error
-		release, err = utils.ParseRelease(runFlags.release)
+		release, err = utils.ParseRelease("", runFlags.release)
 		if err != nil {
 			err := utils.CreateErrorInvalidRelease(executableBase)
 			return err
@@ -115,7 +115,7 @@ func run(cmd *cobra.Command, args []string) error {
 
 	command := args
 
-	container, image, release, err := utils.ResolveContainerAndImageNames(runFlags.container, "", release)
+	container, image, release, err := utils.ResolveContainerAndImageNames(runFlags.container, "", "", release)
 	if err != nil {
 		return err
 	}

commit 2014108f84ff903d6c413cebf8e99c2ffa650a7b

pkg/utils: Mark a private function as such
Figuring out the default toolbox image for a given distribution only
needs to happen as part of resolving the final toolbox image name from
the given command line options.

Fallout from c990fb43cad5f1704584f119ba4f57074c1d0bc9

https://github.com/containers/toolbox/pull/828

src/pkg/utils/utils.go
@@ -285,7 +285,7 @@ func GetContainerNamePrefixForImage(image string) (string, error) {
 	return basename, nil
 }
 
-func GetDefaultImageForDistro(distro, release string) string {
+func getDefaultImageForDistro(distro, release string) string {
 	if _, supportedDistro := supportedDistros[distro]; !supportedDistro {
 		distro = "fedora"
 	}
@@ -720,7 +720,7 @@ func ResolveImageName(distro, image, release string) (string, string, error) {
 	}
 
 	if image == "" {
-		image = GetDefaultImageForDistro(distro, release)
+		image = getDefaultImageForDistro(distro, release)
 	} else {
 		release = ImageReferenceGetTag(image)
 		if release == "" {