containers/toolbox

commit be2ba6d2e203e2d5f42f122df9aea7e16f58e99f

build: Call 'systemd-tmpfiles --create' when installing
It's only necessary to call 'systemd-tmpfiles --create' when building
and installing from source on the host operating system.

It's not needed when using a pre-built binary downstream package,
because:

  * When 'meson install' is called as part of building the package,
    that's not when the temporary files need to be created. They need
    to be created when the binary package is later downloaded and
    installed by the user.

  * Downstream tools can sometimes handle it automatically. eg., on
    Fedora, the systemd RPM installs a trigger that tells RPM to call
    'systemd-tmpfiles --create' automatically when a tmpfiles.d snippet
    is installed.

It's also not needed when installing inside a toolbox container because
the files that 'systemd-tmpfiles --create' is supposed to create are
meant to be on the host.

Downstream distributors set the DESTDIR environment variable when
building their packages. Therefore, it's used to detect when a
downstream package is being built.

Unfortunately, environment variables are messy and, generally, Meson
doesn't support accessing them inside its scripts [1]. Therefore, this
adds a spurious build-time dependency on systemd for downstream
distributors. However, that's probably not a big problem because all
supported downstream operating systems are already expected to use
systemd for the tmpfiles.d(5) snippets to work.

[1] https://github.com/mesonbuild/meson/issues/9

https://github.com/containers/toolbox/issues/955

meson_post_install.py
@@ -1,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+
+destdir = os.environ.get('DESTDIR', '')
+
+if not destdir and not os.path.exists('/run/.containerenv'):
+    print('Calling systemd-tmpfiles --create ...')
+
+    try:
+        subprocess.run(['systemd-tmpfiles', '--create'], check=True)
+    except subprocess.CalledProcessError as e:
+        print('Returned non-zero exit status', e.returncode)
+        sys.exit(e.returncode)
+
+sys.exit(0)

meson.build
@@ -6,6 +6,8 @@ project(
   meson_version: '>= 0.58.0',
 )
 
+fs = import('fs')
+
 cc = meson.get_compiler('c')
 if not cc.has_argument('-print-file-name=libc.so')
   error('C compiler does not support the -print-file-name argument.')
@@ -22,9 +24,12 @@ migration_path_for_coreos_toolbox = get_option('migration_path_for_coreos_toolbo
 profiledir = get_option('profile_dir')
 
 tmpfilesdir = get_option('tmpfiles_dir')
-if tmpfilesdir == ''
+if tmpfilesdir == '' or not fs.exists('/run/.containerenv')
   systemd_dep = dependency('systemd')
-  tmpfilesdir = systemd_dep.get_variable(pkgconfig: 'tmpfilesdir')
+
+  if tmpfilesdir == ''
+    tmpfilesdir = systemd_dep.get_variable(pkgconfig: 'tmpfilesdir')
+  endif
 endif
 
 if bash_completion.found()
@@ -65,3 +70,5 @@ subdir('data')
 subdir('doc')
 subdir('profile.d')
 subdir('src')
+
+meson.add_install_script('meson_post_install.py')

playbooks/setup-env.yaml
@@ -52,10 +52,3 @@
       args:
         chdir: '{{ zuul.project.src_dir }}'
         creates: /usr/local/bin/toolbox
-
-    - name: Setup environment
-      become: yes
-      command:
-        cmd: systemd-tmpfiles --create
-        creates: /run/media
-        creates: /run/host

commit 3f9ee19a66dbdbbe2c2acc992fc8a002112050ee

build: Add copyright and license notice
Fallout from be2ba6d2e203e2d5f42f122df9aea7e16f58e99f

https://github.com/containers/toolbox/pull/984

meson_post_install.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+#
+# Copyright © 2021 – 2022 Red Hat Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 import os
 import subprocess