containers/toolbox

commit a22d7821cb8cea134791dcd4643de21ef565d645

cmd/run: Don't allocate pseudo-TTY if not connected to terminal
Passing '--tty' to 'podman exec' unconditionally causes Podman to
allocate a pseudo-TTY for the command execution. This causes problems
with piping (values not being piped in and values being piped out with
carriage return at the end of a line). The solution is to track the
presence of a terminal on stdin/stdout and based on its presence use the
'--tty' flag.

Original behaviour:
; echo foo | toolbox run less

; toolbox echo foo | od -c
0000000   f   o   o  \r  \n
0000005

New behaviour:
; echo foo | toolbox run less
foo

; toolbox echo foo | od -c
0000000   f   o   o  \n
0000004

As seen in the 'Piping in' example, the value gets only printed into
stdout. Not ideal from the point of view of using 'less' (or similar
tools) but still a move forward.

Based on a discussion in Podman's bugtracker[0].

Fixes https://github.com/containers/toolbox/issues/157
Fixes https://github.com/containers/toolbox/issues/848

[0] https://github.com/containers/podman/issues/9718

https://github.com/containers/toolbox/pull/1013

src/cmd/run.go
@@ -26,6 +26,7 @@ import (
 	"github.com/containers/toolbox/pkg/podman"
 	"github.com/containers/toolbox/pkg/shell"
 	"github.com/containers/toolbox/pkg/utils"
+	"github.com/mattn/go-isatty"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -453,9 +454,12 @@ func constructExecArgs(container string,
 
 	execArgs = append(execArgs, detachKeys...)
 
+	if isatty.IsTerminal(os.Stdin.Fd()) && isatty.IsTerminal(os.Stdout.Fd()) {
+		execArgs = append(execArgs, "--tty")
+	}
+
 	execArgs = append(execArgs, []string{
 		"--interactive",
-		"--tty",
 		"--user", currentUser.Username,
 		"--workdir", workDir,
 	}...)

commit 67849e03a45b5580227e432444c6b0eade865f84

cmd/run: Don't check the stdin and stdout in a loop in the fallback case
The outcome of checking whether the standard input and output of the
current invocation of toolbox are connected to a terminal device is
going to stay constant for the life cycle of the process.  So, checking
it repeatedly in a loop when falling back to a different command or
working directory is wasteful.

Secondly, it prevents secondary logic like this from intermingling with
the code that actually assembles the list of arguments.  This makes it
easier to get a quick gist of the final command and its structure.

Fallout from a22d7821cb8cea134791dcd4643de21ef565d645

src/cmd/run.go
@@ -295,6 +295,18 @@ func runCommandWithFallbacks(container string, command []string, emitEscapeSeque
 
 	envOptions := utils.GetEnvOptionsForPreservedVariables()
 
+	var ttyNeeded bool
+
+	stdinFd := os.Stdin.Fd()
+	stdinFdInt := int(stdinFd)
+
+	stdoutFd := os.Stdout.Fd()
+	stdoutFdInt := int(stdoutFd)
+
+	if term.IsTerminal(stdinFdInt) && term.IsTerminal(stdoutFdInt) {
+		ttyNeeded = true
+	}
+
 	runFallbackCommandsIndex := 0
 	runFallbackWorkDirsIndex := 0
 	workDir := workingDirectory
@@ -305,6 +317,7 @@ func runCommandWithFallbacks(container string, command []string, emitEscapeSeque
 			detachKeysSupported,
 			envOptions,
 			fallbackToBash,
+			ttyNeeded,
 			workDir)
 
 		if emitEscapeSequence {
@@ -444,6 +457,7 @@ func constructExecArgs(container string,
 	detachKeysSupported bool,
 	envOptions []string,
 	fallbackToBash bool,
+	ttyNeeded bool,
 	workDir string) []string {
 	var detachKeys []string
 
@@ -460,13 +474,7 @@ func constructExecArgs(container string,
 
 	execArgs = append(execArgs, detachKeys...)
 
-	stdinFd := os.Stdin.Fd()
-	stdinFdInt := int(stdinFd)
-
-	stdoutFd := os.Stdout.Fd()
-	stdoutFdInt := int(stdoutFd)
-
-	if term.IsTerminal(stdinFdInt) && term.IsTerminal(stdoutFdInt) {
+	if ttyNeeded {
 		execArgs = append(execArgs, "--tty")
 	}
 