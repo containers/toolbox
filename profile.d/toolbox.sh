containers/toolbox

commit 88f2916822f1fdf71caff9d376587adc86b57f20

profile.d: Restore compatibility with Z shell
Otherwise, every zsh instance was running into:
  zsh: #: command not found...

Fallout from dcdfa3a1f5a3421330397ea72c0df132a260853d

https://github.com/containers/toolbox/pull/660

profile.d/toolbox.sh
@@ -6,8 +6,8 @@ host_welcome_stub="$toolbox_config/host-welcome-shown"
 toolbox_welcome_stub="$toolbox_config/toolbox-welcome-shown"
 
 # shellcheck disable=2046
+# shellcheck disable=SC1091
 eval $(
-          # shellcheck disable=SC1091
           . /usr/lib/os-release
 
           echo ID="$ID"

commit 76ef9b3abf8958729105f3ae67e185508afc420e

profile.d: Style fix
Fallout from 88f2916822f1fdf71caff9d376587adc86b57f20 and
dcdfa3a1f5a3421330397ea72c0df132a260853d

https://github.com/containers/toolbox/pull/1176

profile.d/toolbox.sh
@@ -8,8 +8,8 @@ toolbox_config="$HOME/.config/toolbox"
 host_welcome_stub="$toolbox_config/host-welcome-shown"
 toolbox_welcome_stub="$toolbox_config/toolbox-welcome-shown"
 
-# shellcheck disable=2046
 # shellcheck disable=SC1091
+# shellcheck disable=SC2046
 eval $(
           if [ -f /etc/os-release ]; then
               . /etc/os-release