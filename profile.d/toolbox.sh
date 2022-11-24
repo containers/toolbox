containers/toolbox

commit c6e37cdef37e2276207b34a42919f1e0ac4a04dc

profile.d: Tighten the Silverblue check
The Silverblue welcome message was being displayed incorrectly on
other OSTree based OS's (Fedora Atomic Host, Fedora CoreOS, etc).

Note that none of the stable Silverblue releases that have shipped so
far (ie., until Silverblue 30) have had 'silverblue' as the VARIANT_ID.
This makes the check a bit more convoluted that it should have been.

https://github.com/debarshiray/toolbox/pull/236

profile.d/toolbox.sh
@@ -5,8 +5,13 @@ toolbox_config="$HOME/.config/toolbox"
 host_welcome_stub="$toolbox_config/host-welcome-shown"
 toolbox_welcome_stub="$toolbox_config/toolbox-welcome-shown"
 
+# shellcheck disable=SC1091
+. /usr/lib/os-release
+
 if [ -f /run/ostree-booted ] \
-   && ! [ -f "$host_welcome_stub" ]; then
+   && ! [ -f "$host_welcome_stub" ] \
+   && [ "${ID}" = "fedora" ] \
+   && { [ "${VARIANT_ID}" = "workstation" ] || [ "${VARIANT_ID}" = "silverblue" ]; }; then
     echo ""
     echo "Welcome to Fedora Silverblue. This terminal is running on the"
     echo "host system. You may want to try out the Toolbox for a directly"

commit 5249bf229f47a8661ab4b0baeff4c3e1ef81cf3b

profile.d: Don't leak ID and VARIANT_ID into the shell
Commit dcdfa3a1f5a34213 ensured that the rest of the os-release(5)
fields don't get injected into the shell as environment variables, but
missed ID and VARIANT_ID.

Fallout from c6e37cdef37e2276207b34a42919f1e0ac4a04dc

https://github.com/containers/toolbox/pull/1176

profile.d/toolbox.sh
@@ -96,6 +96,8 @@ if [ -f /run/.containerenv ] \
     fi
 fi
 
+unset ID
+unset VARIANT_ID
 unset toolbox_config
 unset host_welcome_stub
 unset toolbox_welcome_stub