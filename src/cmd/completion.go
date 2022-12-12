containers/toolbox

commit 2369da5d31830e5cd3e1a13857a686365875ae61

cmd/list: Filter images/containers using labels
Instead of executing 'podman ps|images' several times in a row, call
them only once and get output with all images/containers. Then, filter
out the JSON using labels and keep images/containers only with matching
labels.

This simplifies the code significantly and cuts down the execution time
of 'toolbox list'. The speed gain is noticeable:

- the system has 5 images and 10 containers

Before patch: ~1.45s
After patch:  ~0.85s

src/cmd/list.go
@@ -51,6 +51,12 @@ var (
 		onlyContainers bool
 		onlyImages     bool
 	}
+
+	// toolboxLabels holds labels used by containers/images that mark them as compatible with Toolbox
+	toolboxLabels = map[string]string{
+		"com.github.debarshiray.toolbox": "true",
+		"com.github.containers.toolbox":  "true",
+	}
 )
 
 var listCmd = &cobra.Command{
@@ -123,35 +129,22 @@ func list(cmd *cobra.Command, args []string) error {
 }
 
 func getContainers() ([]toolboxContainer, error) {
-	logrus.Debug("Fetching containers with label=com.github.containers.toolbox=true")
-	args := []string{"--all", "--filter", "label=com.github.containers.toolbox=true"}
-	containers_old, err := podman.GetContainers(args...)
-	if err != nil {
-		return nil, fmt.Errorf("failed to list containers with label=com.github.containers.toolbox=true: %w", err)
-	}
-
-	logrus.Debug("Fetching containers with label=com.github.debarshiray.toolbox=true")
-	args = []string{"--all", "--filter", "label=com.github.debarshiray.toolbox=true"}
-	containers_new, err := podman.GetContainers(args...)
-	if err != nil {
-		return nil, fmt.Errorf("failed to list containers with label=com.github.debarshiray.toolbox=true: %w", err)
-	}
-
+	var err error
+	var args []string
 	var containers []map[string]interface{}
-	if podman.CheckVersion("2.0.0") {
-		containers = utils.JoinJSON("Id", containers_old, containers_new)
-		containers = utils.SortJSON(containers, "Names", true)
-	} else {
-		containers = utils.JoinJSON("ID", containers_old, containers_new)
-		containers = utils.SortJSON(containers, "Names", false)
+	var toolboxContainers []toolboxContainer
+
+	logrus.Debug("Fetching all containers")
+	args = []string{"--all", "--sort", "names"}
+	containers, err = podman.GetContainers(args...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get containers: %w", err)
 	}
 
-	// This section is a temporary solution that is here to prevent a major
-	// redesign of the way how toolbox containers are fetched.
-	// Remove this in Toolbox v0.2.0
-	var toolboxContainers []toolboxContainer
 	for _, container := range containers {
 		var c toolboxContainer
+		var isToolboxContainer bool = false
+
 		containerJSON, err := json.Marshal(container)
 		if err != nil {
 			logrus.Errorf("failed to marshal container: %v", err)
@@ -163,7 +156,17 @@ func getContainers() ([]toolboxContainer, error) {
 			logrus.Errorf("failed to unmarshal container: %v", err)
 			continue
 		}
-		toolboxContainers = append(toolboxContainers, c)
+
+		for label := range toolboxLabels {
+			if _, ok := c.Labels[label]; ok {
+				isToolboxContainer = true
+				break
+			}
+		}
+
+		if isToolboxContainer {
+			toolboxContainers = append(toolboxContainers, c)
+		}
 	}
 
 	return toolboxContainers, nil
@@ -191,38 +194,22 @@ func listHelp(cmd *cobra.Command, args []string) {
 }
 
 func getImages() ([]toolboxImage, error) {
-	logrus.Debug("Fetching images with label=com.github.containers.toolbox=true")
-	args := []string{"--filter", "label=com.github.containers.toolbox=true"}
-	images_old, err := podman.GetImages(args...)
-	if err != nil {
-		return nil, errors.New("failed to list images with label=com.github.containers.toolbox=true")
-	}
-
-	logrus.Debug("Fetching images with label=com.github.debarshiray.toolbox=true")
-	args = []string{"--filter", "label=com.github.debarshiray.toolbox=true"}
-	images_new, err := podman.GetImages(args...)
-	if err != nil {
-		return nil, errors.New("failed to list images with com.github.debarshiray.toolbox=true")
-	}
-
+	var err error
+	var args []string
 	var images []map[string]interface{}
-	if podman.CheckVersion("2.0.0") {
-		images = utils.JoinJSON("Id", images_old, images_new)
-		images = utils.SortJSON(images, "Names", true)
-	} else if podman.CheckVersion("1.8.3") {
-		images = utils.JoinJSON("ID", images_old, images_new)
-		images = utils.SortJSON(images, "Names", true)
-	} else {
-		images = utils.JoinJSON("id", images_old, images_new)
-		images = utils.SortJSON(images, "names", true)
+	var toolboxImages []toolboxImage
+
+	logrus.Debug("Fetching all images")
+	args = []string{"--sort", "repository"}
+	images, err = podman.GetImages(args...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get images: %w", err)
 	}
 
-	// This section is a temporary solution that is here to prevent a major
-	// redesign of the way how toolbox images are fetched.
-	// Remove this in Toolbox v0.2.0
-	var toolboxImages []toolboxImage
 	for _, image := range images {
 		var i toolboxImage
+		var isToolboxImage bool = false
+
 		imageJSON, err := json.Marshal(image)
 		if err != nil {
 			logrus.Errorf("failed to marshal toolbox image: %v", err)
@@ -234,7 +221,17 @@ func getImages() ([]toolboxImage, error) {
 			logrus.Errorf("failed to unmarshal toolbox image: %v", err)
 			continue
 		}
-		toolboxImages = append(toolboxImages, i)
+
+		for label := range toolboxLabels {
+			if _, ok := i.Labels[label]; ok {
+				isToolboxImage = true
+				break
+			}
+		}
+
+		if isToolboxImage {
+			toolboxImages = append(toolboxImages, i)
+		}
 	}
 
 	return toolboxImages, nil

commit 6aab0a61752dc9a3f0b472b4db5d72c1678ee702

Unbreak sorting and clearly identify copied images in 'list'
Currently, if an image was copied with:
  $ skopeo copy \
      containers-storage:registry.fedoraproject.org/fedora-toolbox:36 \
      containers-storage:localhost/fedora-toolbox:36

... or:
  $ podman tag \
      registry.fedoraproject.org/fedora-toolbox:36 \
      localhost/fedora-toolbox:36

... then it would show up twice in 'list' with the same name, and in the
wrong order.

Either as:
  $ toolbox list --images
  IMAGE ID      IMAGE NAME                                      CREATED
  2110dbbc33d2  localhost/fedora-toolbox:36                     1 day...
  e085805ade4a  registry.access.redhat.com/ubi8/toolbox:latest  1 day...
  2110dbbc33d2  localhost/fedora-toolbox:36                     1 day...
  70cbe2ce60ca  registry.fedoraproject.org/fedora-toolbox:34    1 day...

... or as:
  $ toolbox list --images
  IMAGE ID      IMAGE NAME                                      CREATED
  2110dbbc33d2  registry.fedoraproject.org/fedora-toolbox:36    1 day...
  e085805ade4a  registry.access.redhat.com/ubi8/toolbox:latest  1 day...
  2110dbbc33d2  registry.fedoraproject.org/fedora-toolbox:36    1 day...
  70cbe2ce60ca  registry.fedoraproject.org/fedora-toolbox:34    1 day...

The correct output should be similar to 'podman images', and be sorted
in ascending order of the names:
  $ toolbox list --images
  IMAGE ID      IMAGE NAME                                      CREATED
  2110dbbc33d2  localhost/fedora-toolbox:36                     1 day...
  e085805ade4a  registry.access.redhat.com/ubi8/toolbox:latest  1 day...
  70cbe2ce60ca  registry.fedoraproject.org/fedora-toolbox:34    1 day...
  2110dbbc33d2  registry.fedoraproject.org/fedora-toolbox:36    1 day...

The problem is that, in these situations, 'podman images --format json'
returns separate identical JSON collections for each copy of the image,
and all of those copies have multiple names:
  [
    {
      "Id": "2110dbbc33d2",
      ...
      "Names": [
        "localhost/fedora-toolbox:36",
        "registry.fedoraproject.org/fedora-toolbox:36"
      ],
      ...
    },
    {
      "Id": "e085805ade4a",
      ...
      "Names": [
        "registry.access.redhat.com/ubi8/toolbox:latest"
      ],
      ...
    },
    {
      "Id": "2110dbbc33d2",
      ...
      "Names": [
        "localhost/fedora-toolbox:36",
        "registry.fedoraproject.org/fedora-toolbox:36"
      ],
      ...
    }
    {
      "Id": "70cbe2ce60ca",
      ...
      "Names": [
        "registry.fedoraproject.org/fedora-toolbox:34"
      ],
      ...
    },
  ]

The image objects need to be flattened to have only one unique name per
copy, but with the same ID, and then sorted to ensure the right order.

Note that the ordering was already broken since commit 2369da5d31830e5c,

src/cmd/completion.go
@@ -128,13 +128,13 @@ func completionImageNames(cmd *cobra.Command, _ []string, _ string) ([]string, c
 	}
 
 	var imageNames []string
-	if images, err := getImages(); err == nil {
+	if images, err := getImages(true); err == nil {
 		for _, image := range images {
-			if len(image.Names) > 0 {
-				imageNames = append(imageNames, image.Names[0])
-			} else {
-				imageNames = append(imageNames, image.ID)
+			if len(image.Names) != 1 {
+				panic("cannot complete unflattened Image")
 			}
+
+			imageNames = append(imageNames, image.Names[0])
 		}
 	}
 
@@ -143,19 +143,16 @@ func completionImageNames(cmd *cobra.Command, _ []string, _ string) ([]string, c
 
 func completionImageNamesFiltered(_ *cobra.Command, args []string, _ string) ([]string, cobra.ShellCompDirective) {
 	var imageNames []string
-	if images, err := getImages(); err == nil {
+	if images, err := getImages(true); err == nil {
 		for _, image := range images {
 			skip := false
-			var imageName string
 
-			if len(image.Names) > 0 {
-				imageName = image.Names[0]
-			} else {
-				imageName = image.ID
+			if len(image.Names) != 1 {
+				panic("cannot complete unflattened Image")
 			}
 
 			for _, arg := range args {
-				if arg == imageName {
+				if arg == image.Names[0] {
 					skip = true
 					break
 				}
@@ -165,7 +162,7 @@ func completionImageNamesFiltered(_ *cobra.Command, args []string, _ string) ([]
 				continue
 			}
 
-			imageNames = append(imageNames, imageName)
+			imageNames = append(imageNames, image.Names[0])
 		}
 	}
 

src/cmd/list.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"sort"
 	"text/tabwriter"
 
 	"github.com/containers/toolbox/pkg/podman"
@@ -105,7 +106,7 @@ func list(cmd *cobra.Command, args []string) error {
 	var err error
 
 	if lsImages {
-		images, err = getImages()
+		images, err = getImages(false)
 		if err != nil {
 			return err
 		}
@@ -180,26 +181,41 @@ func listHelp(cmd *cobra.Command, args []string) {
 	}
 }
 
-func getImages() ([]podman.Image, error) {
+func getImages(fillNameWithID bool) ([]podman.Image, error) {
 	logrus.Debug("Fetching all images")
-	args := []string{"--sort", "repository"}
+	var args []string
 	images, err := podman.GetImages(args...)
 	if err != nil {
 		logrus.Debugf("Fetching all images failed: %s", err)
 		return nil, errors.New("failed to get images")
 	}
 
+	processed := make(map[string]struct{})
 	var toolboxImages []podman.Image
 
 	for _, image := range images {
+		if _, ok := processed[image.ID]; ok {
+			continue
+		}
+
+		processed[image.ID] = struct{}{}
+		var isToolboxImage bool
+
 		for label := range toolboxLabels {
 			if _, ok := image.Labels[label]; ok {
-				toolboxImages = append(toolboxImages, image)
+				isToolboxImage = true
 				break
 			}
 		}
+
+		if isToolboxImage {
+			flattenedImages := image.FlattenNames(fillNameWithID)
+			toolboxImages = append(toolboxImages, flattenedImages...)
+		}
+
 	}
 
+	sort.Sort(podman.ImageSlice(toolboxImages))
 	return toolboxImages, nil
 }
 
@@ -209,14 +225,13 @@ func listOutput(images []podman.Image, containers []toolboxContainer) {
 		fmt.Fprintf(writer, "%s\t%s\t%s\n", "IMAGE ID", "IMAGE NAME", "CREATED")
 
 		for _, image := range images {
-			imageName := "<none>"
-			if len(image.Names) != 0 {
-				imageName = image.Names[0]
+			if len(image.Names) != 1 {
+				panic("cannot list unflattened Image")
 			}
 
 			fmt.Fprintf(writer, "%s\t%s\t%s\n",
 				utils.ShortID(image.ID),
-				imageName,
+				image.Names[0],
 				image.Created)
 		}
 

src/cmd/rmi.go
@@ -70,7 +70,7 @@ func rmi(cmd *cobra.Command, args []string) error {
 	}
 
 	if rmiFlags.deleteAll {
-		toolboxImages, err := getImages()
+		toolboxImages, err := getImages(false)
 		if err != nil {
 			return err
 		}

src/pkg/podman/podman.go
@@ -35,6 +35,8 @@ type Image struct {
 	Labels  map[string]string
 }
 
+type ImageSlice []Image
+
 var (
 	podmanVersion string
 )
@@ -42,6 +44,34 @@ var (
 var (
 	LogLevel = logrus.ErrorLevel
 )
+
+func (image *Image) FlattenNames(fillNameWithID bool) []Image {
+	var ret []Image
+
+	if len(image.Names) == 0 {
+		flattenedImage := *image
+
+		if fillNameWithID {
+			shortID := utils.ShortID(image.ID)
+			flattenedImage.Names = []string{shortID}
+		} else {
+			flattenedImage.Names = []string{"<none>"}
+		}
+
+		ret = []Image{flattenedImage}
+		return ret
+	}
+
+	ret = make([]Image, 0, len(image.Names))
+
+	for _, name := range image.Names {
+		flattenedImage := *image
+		flattenedImage.Names = []string{name}
+		ret = append(ret, flattenedImage)
+	}
+
+	return ret
+}
 
 func (image *Image) UnmarshalJSON(data []byte) error {
 	var raw struct {
@@ -70,6 +100,26 @@ func (image *Image) UnmarshalJSON(data []byte) error {
 
 	image.Labels = raw.Labels
 	return nil
+}
+
+func (images ImageSlice) Len() int {
+	return len(images)
+}
+
+func (images ImageSlice) Less(i, j int) bool {
+	if len(images[i].Names) != 1 {
+		panic("cannot sort unflattened ImageSlice")
+	}
+
+	if len(images[j].Names) != 1 {
+		panic("cannot sort unflattened ImageSlice")
+	}
+
+	return images[i].Names[0] < images[j].Names[0]
+}
+
+func (images ImageSlice) Swap(i, j int) {
+	images[i], images[j] = images[j], images[i]
 }
 
 // CheckVersion compares provided version with the version of Podman.

test/system/102-list.bats
@@ -90,8 +90,8 @@ teardown() {
   run --keep-empty-lines --separate-stderr $TOOLBOX list --images
 
   assert_success
-  assert_line --index 1 --partial "$(get_system_id)-toolbox:$(get_system_version)"
-  assert_line --index 2 --partial "fedora-toolbox:34"
+  assert_line --index 1 --partial "fedora-toolbox:34"
+  assert_line --index 2 --partial "$(get_system_id)-toolbox:$(get_system_version)"
   assert [ ${#lines[@]} -eq 4 ]
   if check_bats_version 1.7.0; then
     assert [ ${#stderr_lines[@]} -eq 0 ]
@@ -113,8 +113,8 @@ teardown() {
   run --keep-empty-lines --separate-stderr $TOOLBOX list
 
   assert_success
-  assert_line --index 1 --partial "$(get_system_id)-toolbox:$(get_system_version)"
-  assert_line --index 2 --partial "fedora-toolbox:34"
+  assert_line --index 1 --partial "fedora-toolbox:34"
+  assert_line --index 2 --partial "$(get_system_id)-toolbox:$(get_system_version)"
   assert_line --index 5 --partial "$(get_system_id)-toolbox-$(get_system_version)"
   assert_line --index 6 --partial "non-default-one"
   assert_line --index 7 --partial "non-default-two"
@@ -136,8 +136,8 @@ teardown() {
 
   assert_success
   assert_line --index 1 --partial "<none>"
-  assert_line --index 2 --partial "$default_image"
-  assert_line --index 3 --partial "fedora-toolbox:34"
+  assert_line --index 2 --partial "fedora-toolbox:34"
+  assert_line --index 3 --partial "$default_image"
   assert [ ${#lines[@]} -eq 5 ]
   if check_bats_version 1.7.0; then
     assert [ ${#stderr_lines[@]} -eq 0 ]