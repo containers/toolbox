b091d0a70794bedf95700a94efe79e3117681399
Dawid Kulikowski <git@dawidkulikowski.pl>
2023-05-08 21:21:38 +0200

    Use the --env flag of podman to set HOME to the home of the caller, not root
    Signed-off-by: Dawid Kulikowski <git@dawidkulikowski.pl>

--- src/cmd/create.go
+++ src/cmd/create.go
@@ -426,6 +426,7 @@ func createContainer(container, image, release, authFile string, showCommandToEn
 		"--volume", homeDirMountArg,
 		"--volume", toolboxPathMountArg,
 		"--volume", runtimeDirectoryMountArg,
+		"--env", fmt.Sprintf("HOME=%s", currentUser.HomeDir),
 	}...)
 
 	createArgs = append(createArgs, avahiSocketMount...)
