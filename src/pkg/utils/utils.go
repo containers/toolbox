containers/toolbox

commit df7e01df1005b7f972245ae32b006d35da9f3814

pkg/utils: Ensure that the distro CLI and config file options are valid
Currently, if an invalid or unsupported string is specified as the
distro on the command line or in the configuration file, then it would
silently fallback to Fedora.  This shouldn't happen.

It should only fallback to Fedora when no distro was specified and
there's no supported Toolbox image matching the host operating system.
If a distro was explicitly specified then it should either be supported
or it should error out.

The test cases were resurrected from commit 8b6418d8aa6b1573.

https://github.com/containers/toolbox/issues/937
https://github.com/containers/toolbox/pull/1080

src/pkg/utils/utils.go
@@ -250,10 +250,6 @@ func getDefaultImageForDistro(distro, release string) string {
 		panic("distro not specified")
 	}
 
-	if _, supportedDistro := supportedDistros[distro]; !supportedDistro {
-		distro = distroFallback
-	}
-
 	distroObj, supportedDistro := supportedDistros[distro]
 	if !supportedDistro {
 		panicMsg := fmt.Sprintf("failed to find %s in the list of supported distributions", distro)
@@ -715,6 +711,10 @@ func ResolveContainerAndImageNames(container, distroCLI, imageCLI, releaseCLI st
 		if viper.IsSet("general.distro") {
 			distro = viper.GetString("general.distro")
 		}
+	}
+
+	if _, ok := supportedDistros[distro]; !ok {
+		return "", "", "", fmt.Errorf("distribution %s is unsupported", distro)
 	}
 
 	if distro != distroDefault && releaseCLI == "" && !viper.IsSet("general.release") {

test/system/101-create.bats
@@ -71,6 +71,16 @@ teardown() {
   assert_output --regexp "Created[[:blank:]]+fedora-toolbox-32"
 }
 
+@test "create: Try to create a container based on unsupported distribution" {
+  local distro="foo"
+
+  run $TOOLBOX --assumeyes create --distro "$distro"
+
+  assert_failure
+  assert_line --index 0 "Error: distribution $distro is unsupported"
+  assert [ ${#lines[@]} -eq 1 ]
+}
+
 @test "create: Try to create a container based on non-existent image" {
   run $TOOLBOX -y create -i foo.org/bar
 

test/system/104-run.bats
@@ -48,6 +48,16 @@ teardown() {
   assert_line --index 2 "Run 'toolbox --help' for usage."
 }
 
+@test "run: Try to run a command in a container based on unsupported distribution" {
+  local distro="foo"
+
+  run $TOOLBOX --assumeyes run --distro "$distro" ls
+
+  assert_failure
+  assert_line --index 0 "Error: distribution $distro is unsupported"
+  assert [ ${#lines[@]} -eq 1 ]
+}
+
 @test "run: Try to run a command in a container based on Fedora but with wrong version" {
   run $TOOLBOX run -d fedora -r foobar ls
 

test/system/105-enter.bats
@@ -54,6 +54,16 @@ teardown() {
   assert_line --index 2 "Run 'toolbox --help' for usage."
 }
 
+@test "enter: Try to enter a container based on unsupported distribution" {
+  local distro="foo"
+
+  run $TOOLBOX --assumeyes enter --distro "$distro"
+
+  assert_failure
+  assert_line --index 0 "Error: distribution $distro is unsupported"
+  assert [ ${#lines[@]} -eq 1 ]
+}
+
 @test "enter: Try to enter a container based on Fedora but with wrong version" {
   run $TOOLBOX enter -d fedora -r foobar
 

commit 774ce8bd077908fd401f7ead930e05d12507abe8

pkg/utils: Rename a variable for consistency
Fallout from df7e01df1005b7f972245ae32b006d35da9f3814

https://github.com/containers/toolbox/pull/1303

src/pkg/utils/utils.go
@@ -829,7 +829,7 @@ func ResolveContainerAndImageNames(container, distroCLI, imageCLI, releaseCLI st
 		}
 	}
 
-	if _, ok := supportedDistros[distro]; !ok {
+	if _, supportedDistro := supportedDistros[distro]; !supportedDistro {
 		return "", "", "", &DistroError{distro, ErrDistroUnsupported}
 	}
 