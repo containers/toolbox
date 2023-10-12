containers/toolbox

commit b27795a03ec8ae3139e2dad93b4efca13e96fb01

test/system: Refactor tests using bats-support and bats-assert
The bats-support[0] and bats-assert[1] libraries extend the
capabilities of bats[2]. Mainly, bats-assert is very useful for clean
checking of values/outputs/return codes.

Apart from updating the cases to use the libraries, the test cases have
been restructured in a way that they don't depend on each other anymore.
This required major changes in the helpers.bats file.

Overall, the tests are cleaner to read and easier to extend due to the
test cases being independent.

Some slight changes were made to the test cases themselves. Should not
alter their final behaviour.

There will be a follow up commit that will take care of downloading of
the tested images locally and caching them using Skopeo to speedup the
tests and try to resolve network problems when pulling the images that
we experienced in the past.

[0] https://github.com/bats-core/bats-support
[1] https://github.com/bats-core/bats-assert
[2] https://github.com/bats-core/bats-core

test/system/002-help.bats
@@ -1,8 +1,34 @@
 #!/usr/bin/env bats
 
-load helpers
+load 'libs/bats-support/load'
+load 'libs/bats-assert/load'
 
-@test "Show usage screen when no command is given" {
-  run_toolbox 1
-  is "${lines[0]}" "Error: missing command" "Usage line 1"
+@test "help: Try to run toolbox with no command (shows usage screen)" {
+  run toolbox
+
+  assert_failure
+  assert_line --index 0 "Error: missing command"
+  assert_output --partial "Run 'toolbox --help' for usage."
+}
+
+@test "help: Run command 'help'" {
+  run toolbox help
+
+  assert_success
+  assert_output --partial "toolbox - Unprivileged development environment"
+}
+
+@test "help: Use flag '--help' (it should show usage screen)" {
+  run toolbox --help
+
+  assert_success
+  assert_output --partial "toolbox - Unprivileged development environment"
+}
+
+@test "help: Try to run toolbox with non-existent command (shows usage screen)" {
+  run toolbox foo
+
+  assert_failure
+  assert_line --index 0 "Error: unknown command \"foo\" for \"toolbox\""
+  assert_line --index 1 "Run 'toolbox --help' for usage."
 }

.gitignore
@@ -1,1 +1,3 @@
 src/toolbox
+test/system/libs/bats-assert
+test/system/libs/bats-support

playbooks/fedora-31/setup-env.yaml
@@ -4,6 +4,7 @@
     - name: Install requirements
       become: yes
       package:
+        use: dnf
         name:
           - golang
           - golang-github-cpuguy83-md2man
@@ -15,6 +16,18 @@
           - bash-completion
           - udisks2
           - podman
+
+    - name: Install bats-support library
+      git:
+        repo: https://github.com/bats-core/bats-support
+        dest: '{{ zuul.project.src_dir }}/test/system/libs/bats-support'
+        depth: 1
+
+    - name: Install bats-assert library
+      git:
+        repo: https://github.com/bats-core/bats-assert
+        dest: '{{ zuul.project.src_dir }}/test/system/libs/bats-assert'
+        depth: 1
 
     - name: Setup environment
       become: yes

playbooks/fedora-32/setup-env.yaml
@@ -17,6 +17,18 @@
           - udisks2
           - podman
 
+    - name: Install bats-support library
+      git:
+        repo: https://github.com/bats-core/bats-support
+        dest: '{{ zuul.project.src_dir }}/test/system/libs/bats-support'
+        depth: 1
+
+    - name: Install bats-assert library
+      git:
+        repo: https://github.com/bats-core/bats-assert
+        dest: '{{ zuul.project.src_dir }}/test/system/libs/bats-assert'
+        depth: 1
+
     - name: Setup environment
       become: yes
       command:

playbooks/fedora-33/setup-env.yaml
@@ -17,6 +17,18 @@
           - udisks2
           - podman
 
+    - name: Install bats-support library
+      git:
+        repo: https://github.com/bats-core/bats-support
+        dest: '{{ zuul.project.src_dir }}/test/system/libs/bats-support'
+        depth: 1
+
+    - name: Install bats-assert library
+      git:
+        repo: https://github.com/bats-core/bats-assert
+        dest: '{{ zuul.project.src_dir }}/test/system/libs/bats-assert'
+        depth: 1
+
     - name: Setup environment
       become: yes
       command:

playbooks/fedora-rawhide/setup-env.yaml
@@ -17,6 +17,18 @@
           - udisks2
           - podman
 
+    - name: Install bats-support library
+      git:
+        repo: https://github.com/bats-core/bats-support
+        dest: '{{ zuul.project.src_dir }}/test/system/libs/bats-support'
+        depth: 1
+
+    - name: Install bats-assert library
+      git:
+        repo: https://github.com/bats-core/bats-assert
+        dest: '{{ zuul.project.src_dir }}/test/system/libs/bats-assert'
+        depth: 1
+
     - name: Setup environment
       become: yes
       command:

test/system/001-version.bats
@@ -1,7 +1,10 @@
 #!/usr/bin/env bats
 
-load helpers
+load 'libs/bats-support/load'
+load 'libs/bats-assert/load'
 
-@test "Output version number using full flag" {
-  run_toolbox --version
+@test "version: Check version using option --version" {
+  run toolbox --version
+
+  assert_output --regexp '^toolbox version [0-9]+\.[0-9]+\.[0-9]+$'
 }

test/system/101-create.bats
@@ -1,0 +1,54 @@
+#!/usr/bin/env bats
+
+load 'libs/bats-support/load'
+load 'libs/bats-assert/load'
+load 'libs/helpers'
+
+setup() {
+  cleanup_containers
+}
+
+teardown() {
+  cleanup_containers
+}
+
+
+@test "create: Create the default container" {
+  pull_default_image
+
+  run toolbox -y create
+
+  assert_success
+}
+
+@test "create: Create a container with a valid custom name ('custom-containerName')" {
+  run toolbox -y create -c "custom-containerName"
+
+  assert_success
+}
+
+@test "create: Create a container with a custom image and name ('fedora29'; f29)" {
+  pull_image 29
+
+  run toolbox -y create -c "fedora29" -i fedora-toolbox:29
+
+  assert_success
+}
+
+@test "create: Try to create a container with invalid custom name ('ßpeci@l.Nam€'; using positional argument)" {
+  run toolbox -y create "ßpeci@l.Nam€"
+
+  assert_failure
+  assert_line --index 0 "Error: invalid argument for 'CONTAINER'"
+  assert_line --index 1 "Container names must match '[a-zA-Z0-9][a-zA-Z0-9_.-]*'"
+  assert_line --index 2 "Run 'toolbox --help' for usage."
+}
+
+@test "create: Try to create a container with invalid custom name ('ßpeci@l.Nam€'; using option --container)" {
+  run toolbox -y create -c "ßpeci@l.Nam€"
+
+  assert_failure
+  assert_line --index 0 "Error: invalid argument for '--container'"
+  assert_line --index 1 "Container names must match '[a-zA-Z0-9][a-zA-Z0-9_.-]*'"
+  assert_line --index 2 "Run 'toolbox --help' for usage."
+}

commit 3dc106e10a2bfb7c4bed091221b4d8654f18de20

test/system: Clarify misleading 'toolbox --help' test
Commit 5e63e9ec9bc420da added a 'help' command to show the toolbox(1)
manual or a manual page for a specific command, and made the --help flag
identical to it.  Therefore it's misleading to say that the --help flag
should show the usage screen.  The usage screen is a brief listing of
the commands and options, which isn't the same thing as the more
detailed manuals.

Later, after this test was written, commit 40fc1689a3d90f77 added a
fallback for host operating systems without man(1), like Fedora CoreOS,
that would show a very brief usage screen with only the most common
commands.

To make it more confusing, the test was checking for a string that's
common to both the toolbox(1) manual and the fallback brief usage screen
that might be shown by 'toolbox --help'.  This meant that it was neither
able to distinguish between the code paths nor ensure that they were
working as intended.

This was resolved by adapting the existing 'toolbox --help' test to
strictly ensure that it's showing the toolbox(1) manual when man(1) is
present, and by adding a new test to strictly ensure that it's showing
the fallback brief usage screen when man(1) is absent.

Until Bats 1.10.0, 'run --keep-empty-lines' had a bug where it counted
the trailing newline on the last line as a separate line [1].  However,
Bats 1.10.0 is only available in Fedora >= 39 and is absent from Fedoras
37 and 38.

Fallout from b27795a03ec8ae3139e2dad93b4efca13e96fb01

[1] Bats commit 6648e2143bffb933
    https://github.com/bats-core/bats-core/commit/6648e2143bffb933
    https://github.com/bats-core/bats-core/issues/708

https://github.com/containers/toolbox/pull/1386

test/system/002-help.bats
@@ -62,11 +62,42 @@ setup() {
   assert_line --index 7 "Go to https://github.com/containers/toolbox for further information."
 }
 
-@test "help: Use flag '--help' (it should show usage screen)" {
+@test "help: Use flag '--help'" {
+  if ! command -v man 2>/dev/null; then
+    skip "not found man(1)"
+  fi
+
   run --keep-empty-lines "$TOOLBOX" --help
 
   assert_success
-  assert_output --partial "toolbox - Tool for containerized command line environments on Linux"
+  assert_line --index 0 --partial "toolbox(1)"
+  assert_line --index 0 --partial "General Commands Manual"
+  assert_line --index 3 --partial "toolbox - Tool for containerized command line environments on Linux"
+}
+
+@test "help: Use flag '--help' with man(1) absent" {
+  if command -v man 2>/dev/null; then
+    skip "found man(1)"
+  fi
+
+  run --keep-empty-lines --separate-stderr "$TOOLBOX" --help
+
+  assert_success
+  assert_line --index 0 "toolbox - Tool for containerized command line environments on Linux"
+  assert_line --index 2 "Common commands are:"
+  assert_line --index 3 "create    Create a new toolbox container"
+  assert_line --index 4 "enter     Enter an existing toolbox container"
+  assert_line --index 5 "list      List all existing toolbox containers and images"
+  assert_line --index 7 "Go to https://github.com/containers/toolbox for further information."
+
+  if check_bats_version 1.10.0; then
+    assert [ ${#lines[@]} -eq 8 ]
+  else
+    assert [ ${#lines[@]} -eq 9 ]
+  fi
+
+  # shellcheck disable=SC2154
+  assert [ ${#stderr_lines[@]} -eq 0 ]
 }
 
 @test "help: Try to run toolbox with non-existent command (shows usage screen)" {