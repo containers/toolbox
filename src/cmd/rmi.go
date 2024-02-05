containers/toolbox

commit aaf81a56ea607da03b1f0a764943cca18c80f3df

Sketch out the command line interface in Go
All the existing commands and their flags are present; but barring
'help', '--help' and logging, they don't do anything.

The skeleton was bootstrapped with Cobra Generator.

https://github.com/containers/toolbox/pull/318

src/cmd/rmi.go
@@ -1,0 +1,78 @@
+/*
+ * Copyright © 2019 – 2020 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/containers/toolbox/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	rmiFlags struct {
+		deleteAll   bool
+		forceDelete bool
+	}
+)
+
+var rmiCmd = &cobra.Command{
+	Use:   "rmi",
+	Short: "Remove one or more toolbox images",
+	RunE:  rmi,
+}
+
+func init() {
+	flags := rmiCmd.Flags()
+
+	flags.BoolVarP(&rmiFlags.deleteAll, "all", "a", false, "Remove all toolbox containers.")
+
+	flags.BoolVarP(&rmiFlags.forceDelete,
+		"force",
+		"f",
+		false,
+		"Force the removal of running and paused toolbox containers.")
+
+	rmiCmd.SetHelpFunc(rmiHelp)
+	rootCmd.AddCommand(rmiCmd)
+}
+
+func rmi(cmd *cobra.Command, args []string) error {
+	return nil
+}
+
+func rmiHelp(cmd *cobra.Command, args []string) {
+	if utils.IsInsideContainer() {
+		if !utils.IsInsideToolboxContainer() {
+			fmt.Fprintf(os.Stderr, "Error: this is not a toolbox container\n")
+			return
+		}
+
+		if _, err := utils.ForwardToHost(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			return
+		}
+
+		return
+	}
+
+	if err := utils.ShowManual("toolbox-rmi"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return
+	}
+}

src/cmd/create.go
@@ -1,0 +1,89 @@
+/*
+ * Copyright © 2019 – 2020 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/containers/toolbox/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	createFlags struct {
+		container string
+		image     string
+		release   string
+	}
+)
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new toolbox container",
+	RunE:  create,
+}
+
+func init() {
+	flags := createCmd.Flags()
+
+	flags.StringVarP(&createFlags.container,
+		"container",
+		"c",
+		"",
+		"Assign a different name to the toolbox container.")
+
+	flags.StringVarP(&createFlags.image,
+		"image",
+		"i",
+		"",
+		"Change the name of the base image used to create the toolbox container.")
+
+	flags.StringVarP(&createFlags.release,
+		"release",
+		"r",
+		"",
+		"Create a toolbox container for a different operating system release than the host.")
+
+	createCmd.SetHelpFunc(createHelp)
+	rootCmd.AddCommand(createCmd)
+}
+
+func create(cmd *cobra.Command, args []string) error {
+	return nil
+}
+
+func createHelp(cmd *cobra.Command, args []string) {
+	if utils.IsInsideContainer() {
+		if !utils.IsInsideToolboxContainer() {
+			fmt.Fprintf(os.Stderr, "Error: this is not a toolbox container\n")
+			return
+		}
+
+		if _, err := utils.ForwardToHost(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			return
+		}
+
+		return
+	}
+
+	if err := utils.ShowManual("toolbox-create"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return
+	}
+}

src/cmd/enter.go
@@ -1,0 +1,82 @@
+/*
+ * Copyright © 2019 – 2020 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/containers/toolbox/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	enterFlags struct {
+		container string
+		release   string
+	}
+)
+
+var enterCmd = &cobra.Command{
+	Use:   "enter",
+	Short: "Enter a toolbox container for interactive use",
+	RunE:  enter,
+}
+
+func init() {
+	flags := enterCmd.Flags()
+
+	flags.StringVarP(&enterFlags.container,
+		"container",
+		"c",
+		"",
+		"Enter a toolbox container with the given name.")
+
+	flags.StringVarP(&enterFlags.release,
+		"release",
+		"r",
+		"",
+		"Enter a toolbox container for a different operating system release than the host.")
+
+	enterCmd.SetHelpFunc(enterHelp)
+	rootCmd.AddCommand(enterCmd)
+}
+
+func enter(cmd *cobra.Command, args []string) error {
+	return nil
+}
+
+func enterHelp(cmd *cobra.Command, args []string) {
+	if utils.IsInsideContainer() {
+		if !utils.IsInsideToolboxContainer() {
+			fmt.Fprintf(os.Stderr, "Error: this is not a toolbox container\n")
+			return
+		}
+
+		if _, err := utils.ForwardToHost(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			return
+		}
+
+		return
+	}
+
+	if err := utils.ShowManual("toolbox-enter"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return
+	}
+}

src/cmd/help.go
@@ -1,0 +1,96 @@
+/*
+ * Copyright © 2019 – 2020 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/containers/toolbox/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var helpCmd = &cobra.Command{
+	Use:   "help",
+	Short: "Display help information about Toolbox",
+	RunE:  help,
+}
+
+func init() {
+	helpCmd.SetHelpFunc(helpHelp)
+	rootCmd.AddCommand(helpCmd)
+}
+
+func help(cmd *cobra.Command, args []string) error {
+	if utils.IsInsideContainer() {
+		if !utils.IsInsideToolboxContainer() {
+			return errors.New("this is not a toolbox container")
+		}
+
+		if _, err := utils.ForwardToHost(); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	if err := helpShowManual(args); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func helpHelp(cmd *cobra.Command, args []string) {
+	if utils.IsInsideContainer() {
+		if !utils.IsInsideToolboxContainer() {
+			fmt.Fprintf(os.Stderr, "Error: this is not a toolbox container\n")
+			return
+		}
+
+		if _, err := utils.ForwardToHost(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			return
+		}
+
+		return
+	}
+
+	if err := helpShowManual(args); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return
+	}
+}
+
+func helpShowManual(args []string) error {
+	var manual string
+
+	if len(args) == 0 {
+		manual = "toolbox"
+	} else if args[0] == "toolbox" {
+		manual = "toolbox"
+	} else {
+		manual = "toolbox-" + args[0]
+	}
+
+	if err := utils.ShowManual(manual); err != nil {
+		return err
+	}
+
+	return nil
+}

src/cmd/initContainer.go
@@ -1,0 +1,118 @@
+/*
+ * Copyright © 2019 – 2020 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/containers/toolbox/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	initContainerFlags struct {
+		home        string
+		homeLink    bool
+		mediaLink   bool
+		mntLink     bool
+		monitorHost bool
+		shell       string
+		uid         int
+		user        string
+	}
+)
+
+var initContainerCmd = &cobra.Command{
+	Use:    "init-container",
+	Short:  "Initialize a running container",
+	Hidden: true,
+	RunE:   initContainer,
+}
+
+func init() {
+	flags := initContainerCmd.Flags()
+
+	flags.StringVar(&initContainerFlags.home,
+		"home",
+		"",
+		"Create a user inside the toolbox container whose login directory is HOME.")
+	initContainerCmd.MarkFlagRequired("home")
+
+	flags.BoolVar(&initContainerFlags.homeLink,
+		"home-link",
+		false,
+		"Make /home a symbolic link to /var/home.")
+
+	flags.BoolVar(&initContainerFlags.mediaLink,
+		"media-link",
+		false,
+		"Make /media a symbolic link to /run/media.")
+
+	flags.BoolVar(&initContainerFlags.mntLink, "mnt-link", false, "Make /mnt a symbolic link to /var/mnt.")
+
+	flags.BoolVar(&initContainerFlags.monitorHost,
+		"monitor-host",
+		false,
+		"Ensure that certain configuration files inside the toolbox container are in sync with the host.")
+
+	flags.StringVar(&initContainerFlags.shell,
+		"shell",
+		"",
+		"Create a user inside the toolbox container whose login shell is SHELL.")
+	initContainerCmd.MarkFlagRequired("shell")
+
+	flags.IntVar(&initContainerFlags.uid,
+		"uid",
+		0,
+		"Create a user inside the toolbox container whose numerical user ID is UID.")
+	initContainerCmd.MarkFlagRequired("uid")
+
+	flags.StringVar(&initContainerFlags.user,
+		"user",
+		"",
+		"Create a user inside the toolbox container whose login name is USER.")
+	initContainerCmd.MarkFlagRequired("user")
+
+	initContainerCmd.SetHelpFunc(initContainerHelp)
+	rootCmd.AddCommand(initContainerCmd)
+}
+
+func initContainer(cmd *cobra.Command, args []string) error {
+	return nil
+}
+
+func initContainerHelp(cmd *cobra.Command, args []string) {
+	if utils.IsInsideContainer() {
+		if !utils.IsInsideToolboxContainer() {
+			fmt.Fprintf(os.Stderr, "Error: this is not a toolbox container\n")
+			return
+		}
+
+		if _, err := utils.ForwardToHost(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			return
+		}
+
+		return
+	}
+
+	if err := utils.ShowManual("toolbox-init-container"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return
+	}
+}

src/cmd/list.go
@@ -1,0 +1,82 @@
+/*
+ * Copyright © 2019 – 2020 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/containers/toolbox/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	listFlags struct {
+		onlyContainers bool
+		onlyImages     bool
+	}
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List existing toolbox containers and images",
+	RunE:  list,
+}
+
+func init() {
+	flags := listCmd.Flags()
+
+	flags.BoolVarP(&listFlags.onlyContainers,
+		"containers",
+		"c",
+		false,
+		"List only toolbox containers, not images.")
+
+	flags.BoolVarP(&listFlags.onlyImages,
+		"images",
+		"i",
+		false,
+		"List only toolbox images, not containers.")
+
+	listCmd.SetHelpFunc(listHelp)
+	rootCmd.AddCommand(listCmd)
+}
+
+func list(cmd *cobra.Command, args []string) error {
+	return nil
+}
+
+func listHelp(cmd *cobra.Command, args []string) {
+	if utils.IsInsideContainer() {
+		if !utils.IsInsideToolboxContainer() {
+			fmt.Fprintf(os.Stderr, "Error: this is not a toolbox container\n")
+			return
+		}
+
+		if _, err := utils.ForwardToHost(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			return
+		}
+
+		return
+	}
+
+	if err := utils.ShowManual("toolbox-list"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return
+	}
+}

src/cmd/reset.go
@@ -1,0 +1,61 @@
+/*
+ * Copyright © 2019 – 2020 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/containers/toolbox/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var resetCmd = &cobra.Command{
+	Use:   "reset",
+	Short: "Remove all local podman (and toolbox) state",
+	RunE:  reset,
+}
+
+func init() {
+	resetCmd.SetHelpFunc(resetHelp)
+	rootCmd.AddCommand(resetCmd)
+}
+
+func reset(cmd *cobra.Command, args []string) error {
+	return nil
+}
+
+func resetHelp(cmd *cobra.Command, args []string) {
+	if utils.IsInsideContainer() {
+		if !utils.IsInsideToolboxContainer() {
+			fmt.Fprintf(os.Stderr, "Error: this is not a toolbox container\n")
+			return
+		}
+
+		if _, err := utils.ForwardToHost(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			return
+		}
+
+		return
+	}
+
+	if err := utils.ShowManual("toolbox-reset"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return
+	}
+}

src/cmd/rm.go
@@ -1,0 +1,78 @@
+/*
+ * Copyright © 2019 – 2020 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/containers/toolbox/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	rmFlags struct {
+		deleteAll   bool
+		forceDelete bool
+	}
+)
+
+var rmCmd = &cobra.Command{
+	Use:   "rm",
+	Short: "Remove one or more toolbox containers",
+	RunE:  rm,
+}
+
+func init() {
+	flags := rmCmd.Flags()
+
+	flags.BoolVarP(&rmFlags.deleteAll, "all", "a", false, "Remove all toolbox containers.")
+
+	flags.BoolVarP(&rmFlags.forceDelete,
+		"force",
+		"f",
+		false,
+		"Force the removal of running and paused toolbox containers.")
+
+	rmCmd.SetHelpFunc(rmHelp)
+	rootCmd.AddCommand(rmCmd)
+}
+
+func rm(cmd *cobra.Command, args []string) error {
+	return nil
+}
+
+func rmHelp(cmd *cobra.Command, args []string) {
+	if utils.IsInsideContainer() {
+		if !utils.IsInsideToolboxContainer() {
+			fmt.Fprintf(os.Stderr, "Error: this is not a toolbox container\n")
+			return
+		}
+
+		if _, err := utils.ForwardToHost(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+			return
+		}
+
+		return
+	}
+
+	if err := utils.ShowManual("toolbox-rm"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return
+	}
+}

commit da23002c39e639b70ee6f902d275ec49f6dc4b22

cmd/rmi: Fix typo
Fallout from aaf81a56ea607da03b1f0a764943cca18c80f3df

https://github.com/containers/toolbox/pull/1446

src/cmd/rmi.go
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019 – 2022 Red Hat Inc.
+ * Copyright © 2019 – 2024 Red Hat Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,13 +44,13 @@ var rmiCmd = &cobra.Command{
 func init() {
 	flags := rmiCmd.Flags()
 
-	flags.BoolVarP(&rmiFlags.deleteAll, "all", "a", false, "Remove all toolbox containers")
+	flags.BoolVarP(&rmiFlags.deleteAll, "all", "a", false, "Remove all toolbox images")
 
 	flags.BoolVarP(&rmiFlags.forceDelete,
 		"force",
 		"f",
 		false,
-		"Force the removal of running and paused toolbox containers")
+		"Force the removal of toolbox images that are used by toolbox containers")
 
 	rmiCmd.SetHelpFunc(rmiHelp)
 	rootCmd.AddCommand(rmiCmd)