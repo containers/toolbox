27878880e619c6760e86fc14817918dd0382403a
Debarshi Ray <rishi@fedoraproject.org>
2024-02-28 10:50:36 +0100

    Update copyright notices
    https://github.com/containers/toolbox/pull/1462

--- src/pkg/shell/shell.go
+++ src/pkg/shell/shell.go
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019 – 2023 Red Hat Inc.
+ * Copyright © 2019 – 2024 Red Hat Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
