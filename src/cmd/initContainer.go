containers/toolbox

commit e1635c06f3d2038097c875b7b1c293c7bc40bb1b

Make locate(1) work inside toolbox containers
... by running updatedb(8) on start-up and then at 24 hour intervals
from there on.

This isn't as nice as using a systemd.timer(5) because the current
timer goes away when the toolbox container is stopped and is rearmed
when it's started. Therefore, repeatedly restarting a container will
also run updatedb(8) again and again.

Fortunately, this isn't so bad with updatedb(5) implementations that
are able to incrementally update the database [1], which is what Fedora
uses.

The 24 hour interval was chosen based on the systemd.timer(5) settings
used by Fedora's mlocate RPM.

[1] https://pagure.io/mlocate

https://github.com/containers/toolbox/issues/391

src/cmd/initContainer.go
@@ -24,6 +24,7 @@ import (
 	"os/user"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/containers/toolbox/pkg/shell"
 	"github.com/containers/toolbox/pkg/utils"
@@ -258,6 +259,17 @@ func initContainer(cmd *cobra.Command, args []string) error {
 		}
 	}
 
+	logrus.Debug("Setting up daily ticker")
+
+	daily, err := time.ParseDuration("24h")
+	if err != nil {
+		panicMsg := fmt.Sprintf("failed to parse duration: %v", err)
+		panic(panicMsg)
+	}
+
+	tickerDaily := time.NewTicker(daily)
+	defer tickerDaily.Stop()
+
 	logrus.Debug("Setting up watches for file system events")
 
 	watcherForHost, err := fsnotify.NewWatcher()
@@ -301,10 +313,14 @@ func initContainer(cmd *cobra.Command, args []string) error {
 		return errors.New("failed to change ownership of initialization stamp")
 	}
 
-	logrus.Debug("Listening to file system events")
+	logrus.Debug("Listening to file system and ticker events")
+
+	go runUpdateDb()
 
 	for {
 		select {
+		case event := <-tickerDaily.C:
+			handleDailyTick(event)
 		case event := <-watcherForHost.Events:
 			handleFileSystemEvent(event)
 		case err := <-watcherForHost.Errors:
@@ -405,6 +421,13 @@ func configureUsers(targetUserUid int,
 	}
 
 	return nil
+}
+
+func handleDailyTick(event time.Time) {
+	eventString := event.String()
+	logrus.Debugf("Handling daily tick %s", eventString)
+
+	runUpdateDb()
 }
 
 func handleFileSystemEvent(event fsnotify.Event) {
@@ -514,6 +537,12 @@ func redirectPath(containerPath, target string, folder bool) error {
 	}
 
 	return nil
+}
+
+func runUpdateDb() {
+	if err := shell.Run("updatedb", nil, nil, nil); err != nil {
+		logrus.Warnf("Failed to run updatedb(8): %v", err)
+	}
 }
 
 func sanitizeRedirectionTarget(target string) string {

test/system/helpers.bash
@@ -113,7 +113,7 @@ function run_toolbox() {
 
 function is_toolbox_ready() {
     toolbox_container="$1"
-    expected_string="Listening to file system events"
+    expected_string="Listening to file system and ticker events"
     num_of_tries=5
     timeout=2
 

commit 8c0064a305be1ff42e670435c554fedeaf98acf5

cmd/initContainer: Simplify creating a daily ticker
Fallout from e1635c06f3d2038097c875b7b1c293c7bc40bb1b

https://github.com/containers/toolbox/pull/1494

src/cmd/initContainer.go
@@ -286,13 +286,7 @@ func initContainer(cmd *cobra.Command, args []string) error {
 
 	logrus.Debug("Setting up daily ticker")
 
-	daily, err := time.ParseDuration("24h")
-	if err != nil {
-		panicMsg := fmt.Sprintf("failed to parse duration: %v", err)
-		panic(panicMsg)
-	}
-
-	tickerDaily := time.NewTicker(daily)
+	tickerDaily := time.NewTicker(24 * time.Hour)
 	defer tickerDaily.Stop()
 
 	logrus.Debug("Setting up watches for file system events")