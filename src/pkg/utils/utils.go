fb43d3e06e2d70fe2d2c44bcb58c3bed331a5575
Debarshi Ray <rishi@fedoraproject.org>
2025-07-04 21:03:24 +0200

    pkg/utils: Remove unused functions
    Fallout from d323143c468e5e59dba4d209a4e4767d88dfc19e
    
    https://github.com/containers/toolbox/pull/1677

--- src/pkg/utils/utils.go
+++ src/pkg/utils/utils.go
@@ -473,38 +473,6 @@ func GetInitializedStamp(entryPointPID int, targetUser *user.User) (string, erro
 	return initializedStamp, nil
 }
 
-// GetMountPoint returns the mount point of a target.
-func GetMountPoint(target string) (string, error) {
-	var stdout strings.Builder
-	if err := shell.Run("df", nil, &stdout, nil, "--output=target", target); err != nil {
-		return "", err
-	}
-
-	output := stdout.String()
-	options := strings.Split(output, "\n")
-	if len(options) != 3 {
-		return "", errors.New("unexpected output from df(1)")
-	}
-
-	mountPoint := strings.TrimSpace(options[1])
-	return mountPoint, nil
-}
-
-// GetMountOptions returns the mount options of a target.
-func GetMountOptions(target string) (string, error) {
-	var stdout strings.Builder
-	findMntArgs := []string{"--noheadings", "--output", "OPTIONS", target}
-
-	if err := shell.Run("findmnt", nil, &stdout, nil, findMntArgs...); err != nil {
-		return "", err
-	}
-
-	output := stdout.String()
-	options := strings.Split(output, "\n")
-	mountOptions := strings.TrimSpace(options[0])
-	return mountOptions, nil
-}
-
 func GetP11KitServerSocket(targetUser *user.User) (string, error) {
 	toolbxRuntimeDirectory, err := GetRuntimeDirectory(targetUser)
 	if err != nil {
