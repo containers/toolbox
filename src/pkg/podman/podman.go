containers/toolbox

commit 2ecaaf8ba3e1061755757a97333e93803622bb35

cmd/rm, pkg/podman: Move 'removeContainer' func to podman pkg
The 'removeContainer' function should go into 'pkg/podman' because it
wraps around Podman's command. Because it no longer has access to the
commands - 'toolbox rm' - parameters it has a new forceDelete parameter.

https://github.com/containers/toolbox/pull/519

src/pkg/podman/podman.go
@@ -240,6 +240,39 @@ func Pull(imageName string) error {
 	return nil
 }
 
+func RemoveContainer(container string, forceDelete bool) error {
+	logrus.Debugf("Removing container %s", container)
+
+	logLevelString := LogLevel.String()
+	args := []string{"--log-level", logLevelString, "rm"}
+
+	if forceDelete {
+		args = append(args, "--force")
+	}
+
+	args = append(args, container)
+
+	exitCode, err := shell.RunWithExitCode("podman", nil, nil, nil, args...)
+	switch exitCode {
+	case 0:
+		if err != nil {
+			panic("unexpected error: 'podman rm' finished successfully")
+		}
+	case 1:
+		err = fmt.Errorf("container %s does not exist", container)
+	case 2:
+		err = fmt.Errorf("container %s is running", container)
+	default:
+		err = fmt.Errorf("failed to remove container %s", container)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func SetLogLevel(logLevel logrus.Level) {
 	LogLevel = logLevel
 }

src/cmd/rm.go
@@ -23,7 +23,6 @@ import (
 	"strings"
 
 	"github.com/containers/toolbox/pkg/podman"
-	"github.com/containers/toolbox/pkg/shell"
 	"github.com/containers/toolbox/pkg/utils"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -96,7 +95,7 @@ func rm(cmd *cobra.Command, args []string) error {
 
 		for _, container := range containers {
 			containerID := container[idKey].(string)
-			if err := removeContainer(containerID); err != nil {
+			if err := podman.RemoveContainer(containerID, rmFlags.forceDelete); err != nil {
 				fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 				continue
 			}
@@ -117,7 +116,7 @@ func rm(cmd *cobra.Command, args []string) error {
 				continue
 			}
 
-			if err := removeContainer(container); err != nil {
+			if err := podman.RemoveContainer(container, rmFlags.forceDelete); err != nil {
 				fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 				continue
 			}
@@ -146,37 +145,4 @@ func rmHelp(cmd *cobra.Command, args []string) {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		return
 	}
-}
-
-func removeContainer(container string) error {
-	logrus.Debugf("Removing container %s", container)
-
-	logLevelString := podman.LogLevel.String()
-	args := []string{"--log-level", logLevelString, "rm"}
-
-	if rmFlags.forceDelete {
-		args = append(args, "--force")
-	}
-
-	args = append(args, container)
-
-	exitCode, err := shell.RunWithExitCode("podman", nil, nil, nil, args...)
-	switch exitCode {
-	case 0:
-		if err != nil {
-			panic("unexpected error: 'podman rm' finished successfully")
-		}
-	case 1:
-		err = fmt.Errorf("container %s does not exist", container)
-	case 2:
-		err = fmt.Errorf("container %s is running", container)
-	default:
-		err = fmt.Errorf("failed to remove container %s", container)
-	}
-
-	if err != nil {
-		return err
-	}
-
-	return nil
 }

commit 106f410fab5118a9f755c16ec55297e3e3cbd4da

pkg/podman: Tweak the error messages for consistency
Fallout from 2ecaaf8ba3e1061755757a97333e93803622bb35 and
ff1fab0f52eb4b162e2ab039d2044fe5ec386439

https://github.com/containers/toolbox/pull/1711

src/pkg/podman/podman.go
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019 – 2024 Red Hat Inc.
+ * Copyright © 2019 – 2025 Red Hat Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -431,7 +431,7 @@ func RemoveContainer(container string, forceDelete bool) error {
 			panic("unexpected error: 'podman rm' finished successfully")
 		}
 	case 1:
-		err = fmt.Errorf("container %s does not exist", container)
+		err = fmt.Errorf("container %s not found", container)
 	case 2:
 		err = fmt.Errorf("container %s is running", container)
 	default:
@@ -464,7 +464,7 @@ func RemoveImage(image string, forceDelete bool) error {
 			panic("unexpected error: 'podman rmi' finished successfully")
 		}
 	case 1:
-		err = fmt.Errorf("image %s does not exist", image)
+		err = fmt.Errorf("image %s not found", image)
 	case 2:
 		err = fmt.Errorf("image %s has dependent children", image)
 	default: