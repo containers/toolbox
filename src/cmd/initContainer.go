containers/toolbox

commit 5ed2442214f0eb8d58d1bfc7df50f62328815cb2

Give access to the CA certificates from the host
This uses the same approach taken by Flatpak [1] to ensure that the
certificates from certificate authorities (or CAs) that are available
inside a Toolbx container are kept synchronized with the host operating
system.  Any program that uses PKCS #11 to access CA certificates should
see the same ones both inside the container and on the host.

During every 'enter' and 'run' command, toolbox(1) ensures that an
instance of 'p11-kit server' is running on the host listening on a local
file system socket that's accessible to both the container and the host.
If an instance is already running, then a second one is not created.
The location of the socket is injected into the container through the
P11_KIT_SERVER_ADDRESS environment variable.

Just like Flatpak, the singleton 'p11-kit server' process is not
terminated when the last 'enter' or 'run' command exits.

The Toolbx container's entry point configures it to use the
p11-kit-client.so PKCS #11 module instead of the usual p11-kit-trust.so
module.  This talks to the 'p11-kit server' instance running on the host
over the socket instead of reading the CA certificates that are present
inside the container.

However, unlike Flatpak, this doesn't use D-Bus to set up the
communication between the container and the host, because when invoked
as 'sudo toolbox ...' there's no user or session D-Bus instance
available for the root user.

This set-up is skipped if 'p11-kit server' can't be run on the host, or
if the /etc/pkcs11/modules directory for configuring PKCS #11 modules or
p11-kit-client.so are missing inside the container.  None of these are
considered hard dependencies to accommodate size-constrained OSes like
Fedora CoreOS that might not have 'p11-kit server', and existing Toolbx
containers and old images that might not have p11-kit-client.so.

The UBI-based toolbox images haven't yet been updated to contain

src/cmd/initContainer.go
@@ -301,6 +301,10 @@ func initContainer(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
+	if err := configurePKCS11(targetUser); err != nil {
+		return err
+	}
+
 	if err := configureRPM(); err != nil {
 		return err
 	}
@@ -564,6 +568,57 @@ func configureKerberos() error {
 	kcmConfigBytes := []byte(kcmConfigString)
 	if err := ioutil.WriteFile("/etc/krb5.conf.d/kcm_default_ccache", kcmConfigBytes, 0644); err != nil {
 		return errors.New("failed to configure Kerberos to use KCM as the default credential cache")
+	}
+
+	return nil
+}
+
+func configurePKCS11(targetUser *user.User) error {
+	const logPrefix = "Configuring PKCS #11 to read from the host"
+	logrus.Debugf("%s", logPrefix)
+
+	if path := "/etc/pkcs11/modules"; !utils.PathExists(path) {
+		logrus.Debugf("%s: directory %s not found", logPrefix, path)
+		logrus.Debugf("%s: skipping", logPrefix)
+		return nil
+	}
+
+	if ok, err := utils.IsP11KitClientPresent(); err != nil {
+		logrus.Debugf("%s: %s", logPrefix, err)
+
+		if !ok {
+			logrus.Debugf("%s: p11-kit-client.so not found", logPrefix)
+			logrus.Debugf("%s: skipping", logPrefix)
+			return nil
+		}
+	} else {
+		if !ok {
+			logrus.Debugf("%s: p11-kit-client.so not found", logPrefix)
+			logrus.Debugf("%s: skipping", logPrefix)
+			return nil
+		}
+	}
+
+	if path, err := utils.GetP11KitServerSocket(targetUser); err != nil {
+		return err
+	} else if !utils.PathExists(path) {
+		logrus.Debugf("%s: socket %s not found", logPrefix, path)
+		logrus.Debugf("%s: skipping", logPrefix)
+		return nil
+	}
+
+	var builder strings.Builder
+	builder.WriteString("# Written by Toolbx\n")
+	builder.WriteString("# https://containertoolbx.org/\n")
+	builder.WriteString("\n")
+	builder.WriteString("module: p11-kit-client.so\n")
+
+	pkcs11ConfigString := builder.String()
+	pkcs11ConfigBytes := []byte(pkcs11ConfigString)
+	if err := renameio.WriteFile("/etc/pkcs11/modules/p11-kit-trust.module",
+		pkcs11ConfigBytes,
+		0644); err != nil {
+		return fmt.Errorf("failed to configure PKCS #11 to read from the host: %w", err)
 	}
 
 	return nil

src/pkg/utils/utils.go
@@ -503,6 +503,26 @@ func GetMountOptions(target string) (string, error) {
 	return mountOptions, nil
 }
 
+func GetP11KitServerSocket(targetUser *user.User) (string, error) {
+	toolbxRuntimeDirectory, err := GetRuntimeDirectory(targetUser)
+	if err != nil {
+		return "", err
+	}
+
+	p11KitServerSocket := filepath.Join(toolbxRuntimeDirectory, "pkcs11")
+	return p11KitServerSocket, nil
+}
+
+func GetP11KitServerSocketLock(targetUser *user.User) (string, error) {
+	toolbxRuntimeDirectory, err := GetRuntimeDirectory(targetUser)
+	if err != nil {
+		return "", err
+	}
+
+	p11KitServerSocketLock := filepath.Join(toolbxRuntimeDirectory, "pkcs11.lock")
+	return p11KitServerSocketLock, nil
+}
+
 func GetRuntimeDirectory(targetUser *user.User) (string, error) {
 	if runtimeDirectories == nil {
 		runtimeDirectories = make(map[string]string)

.github/workflows/ubuntu-tests.yaml
@@ -1,5 +1,5 @@
 #
-# Copyright © 2023 – 2024 Red Hat, Inc.
+# Copyright © 2023 – 2025 Red Hat, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -54,6 +54,9 @@ jobs:
             skopeo \
             systemd \
             udisks2
+
+      - name: Ensure that 'p11-kit server' is absent
+        run: sudo rm /usr/libexec/p11-kit/p11-kit-server
 
       - name: Set up PATH for Go 1.21
         run: |
@@ -131,7 +134,7 @@ jobs:
         working-directory: containers/toolbox/src
 
       - name: Set up build directory
-        run: meson setup --fatal-meson-warnings builddir
+        run: meson setup builddir
         working-directory: containers/toolbox
 
       - name: Build

meson.build
@@ -23,6 +23,27 @@ bats = find_program('bats', required: false)
 codespell = find_program('codespell', required: false)
 htpasswd = find_program('htpasswd', required: false)
 openssl = find_program('openssl', required: false)
+
+p11kit_server_works = false
+p11kit = find_program('p11-kit', required: false)
+if p11kit.found()
+  res = run_command(p11kit, 'server', check: false)
+  if res.returncode() == 0
+    error('Command \'p11-kit server\' was supposed to fail')
+  endif
+
+  res_stdout = res.stdout()
+  if res_stdout.contains('--name') and res_stdout.contains('--provider')
+    p11kit_server_works = true
+  else
+    warning('Command \'p11-kit server\' doesn\'t work')
+  endif
+endif
+
+if not p11kit_server_works
+  warning('Containers won\'t have access to the CA certificates from the host')
+endif
+
 podman = find_program('podman', required: false)
 shellcheck = find_program('shellcheck', required: false)
 skopeo = find_program('skopeo', required: false)

playbooks/dependencies-centos-9-stream.yaml
@@ -38,6 +38,13 @@
       - codespell
       - fish
 
+- name: Ensure that 'p11-kit server' is absent
+  become: yes
+  package:
+    name:
+      - p11-kit-server
+    state: absent
+
 - name: Download Go modules
   command: go mod download -x
   environment:

playbooks/dependencies-fedora-restricted.yaml
@@ -1,5 +1,5 @@
 #
-# Copyright © 2023 – 2024 Red Hat, Inc.
+# Copyright © 2023 – 2025 Red Hat, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -44,6 +44,15 @@
       - systemd
       - udisks2
     state: present
+    update_cache: "{{ true if zuul.attempts > 1 else false }}"
+    use: "{{ 'dnf' if zuul.attempts > 1 else 'auto' }}"
+
+- name: Ensure that 'p11-kit server' is absent
+  become: yes
+  package:
+    name:
+      - p11-kit-server
+    state: absent
     update_cache: "{{ true if zuul.attempts > 1 else false }}"
     use: "{{ 'dnf' if zuul.attempts > 1 else 'auto' }}"
 

playbooks/dependencies-fedora.yaml
@@ -1,5 +1,5 @@
 #
-# Copyright © 2022 – 2024 Red Hat, Inc.
+# Copyright © 2022 – 2025 Red Hat, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -37,6 +37,15 @@
       - skopeo
       - systemd
       - udisks2
+    use: "{{ 'dnf' if zuul.attempts > 1 else 'auto' }}"
+
+- name: Ensure that 'p11-kit server' is absent
+  become: yes
+  package:
+    name:
+      - p11-kit-server
+    state: absent
+    update_cache: "{{ true if zuul.attempts > 1 else false }}"
     use: "{{ 'dnf' if zuul.attempts > 1 else 'auto' }}"
 
 - name: Download Go modules

playbooks/setup-env-migration-path-for-coreos-toolbox.yaml
@@ -20,6 +20,6 @@
     - include_tasks: dependencies-centos-9-stream.yaml
 
     - name: Set up build directory
-      command: meson -Dmigration_path_for_coreos_toolbox=true --fatal-meson-warnings builddir
+      command: meson -Dmigration_path_for_coreos_toolbox=true builddir
       args:
         chdir: '{{ zuul.project.src_dir }}'

commit 851537d37c76c93a3f3f389de4c8e0bc5ed50dc2

cmd/initContainer: Unbreak access to CA certificates in su(1) & sudo(8)
When a Toolbx container is set up to use the p11-kit-client.so PKCS #11
module instead of the usual p11-kit-trust.so module, the
P11_KIT_SERVER_ADDRESS environment variable must be set inside the
container, so that it can communicate with the host operating system.

Currently, this works as described above with the 'enter' and 'run'
commands, but not within child sessions started by su(1) [1] and
sudo(8) [2], because P11_KIT_SERVER_ADDRESS is absent.

As a result, when 'trust extract' [3] or update-ca-trust(8) [4] is
invoked as root, several files within /etc/ca-certificates/extracted in
Arch Linux containers or /etc/pki/ca-trust/extracted in Fedora
containers are either emptied out or truncated.  This can happen when
the package manager inside the container notices an update to the
certificates from certificate authorities (or CAs) or the user wants to
add a custom certificate inside the container.

The empty and truncated files in /etc/ca-certificates/extracted or
/etc/pki/ca-trust/extracted break OpenSSL and other cryptographic
components that don't use PKCS #11 to access CA certificates.

To prevent this, P11_KIT_SERVER_ADDRESS must be set in child sessions
started by su(1) and sudo(8).

Fallout from 5ed2442214f0eb8d58d1bfc7df50f62328815cb2

[1] https://www.man7.org/linux/man-pages/man1/su.1.html

[2] https://man7.org/linux/man-pages/man8/sudo.8.html

[3] https://p11-glue.github.io/p11-glue/p11-kit/manual/trust.html

[4] https://www.systutorials.com/docs/linux/man/8-update-ca-trust/

https://github.com/containers/toolbox/issues/626
https://github.com/containers/toolbox/issues/1661

src/cmd/initContainer.go
@@ -627,6 +627,14 @@ func configurePKCS11(targetUser *user.User) error {
 		return err
 	}
 
+	if err := configurePKCS11Su(serverSocket); err != nil {
+		return err
+	}
+
+	if err := configurePKCS11Sudo(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -670,6 +678,88 @@ func configurePKCS11SSHD(serverSocket string) error {
 
 	if err := renameio.WriteFile(sshdConfig, sshdConfigBytes, filePerm); err != nil {
 		return fmt.Errorf("failed to configure sshd(8) to set P11_KIT_SERVER_ADDRESS: %w", err)
+	}
+
+	return nil
+}
+
+func configurePKCS11Su(serverSocket string) error {
+	const logPrefix = "Configuring su(1) to set P11_KIT_SERVER_ADDRESS"
+	logrus.Debugf("%s", logPrefix)
+
+	const profileD = "/etc/profile.d"
+	fileInfo, err := os.Stat(profileD)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			err := createErrorProfileDNotFound()
+			return err
+		} else {
+			logrus.Debugf("%s: failed to stat %s: %s", logPrefix, profileD, err)
+			return fmt.Errorf("failed to stat %s", profileD)
+		}
+	}
+
+	fileMode := fileInfo.Mode()
+	if !fileMode.IsDir() {
+		err := createErrorProfileDNotFound()
+		return err
+	}
+
+	profile := filepath.Join(profileD, "toolbx-pkcs11.sh")
+
+	var builder strings.Builder
+	fmt.Fprintf(&builder, "# shellcheck shell=sh\n")
+	fmt.Fprintf(&builder, "#\n")
+	fmt.Fprintf(&builder, "# Written by Toolbx\n")
+	fmt.Fprintf(&builder, "# https://containertoolbx.org/\n")
+	fmt.Fprintf(&builder, "\n")
+	fmt.Fprintf(&builder, "export P11_KIT_SERVER_ADDRESS=unix:path=%s\n", serverSocket)
+
+	profileString := builder.String()
+	profileBytes := []byte(profileString)
+
+	if err := renameio.WriteFile(profile, profileBytes, 0644); err != nil {
+		return fmt.Errorf("failed to configure su(1) to set P11_KIT_SERVER_ADDRESS: %w", err)
+	}
+
+	return nil
+}
+
+func configurePKCS11Sudo() error {
+	const logPrefix = "Configuring sudo(8) to set P11_KIT_SERVER_ADDRESS"
+	logrus.Debugf("%s", logPrefix)
+
+	const sudoersD = "/etc/sudoers.d"
+	fileInfo, err := os.Stat(sudoersD)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			err := createErrorSudoersDNotFound()
+			return err
+		} else {
+			logrus.Debugf("%s: failed to stat %s: %s", logPrefix, sudoersD, err)
+			return fmt.Errorf("failed to stat %s", sudoersD)
+		}
+	}
+
+	fileMode := fileInfo.Mode()
+	if !fileMode.IsDir() {
+		err := createErrorSudoersDNotFound()
+		return err
+	}
+
+	sudoers := filepath.Join(sudoersD, "90-toolbx-pkcs11")
+
+	var builder strings.Builder
+	builder.WriteString("# Written by Toolbx\n")
+	builder.WriteString("# https://containertoolbx.org/\n")
+	builder.WriteString("\n")
+	builder.WriteString("Defaults env_keep += \"P11_KIT_SERVER_ADDRESS\"\n")
+
+	sudoersString := builder.String()
+	sudoersBytes := []byte(sudoersString)
+
+	if err := renameio.WriteFile(sudoers, sudoersBytes, 0440); err != nil {
+		return fmt.Errorf("failed to configure sudo(8) to set P11_KIT_SERVER_ADDRESS: %w", err)
 	}
 
 	return nil

src/cmd/utils.go
@@ -331,6 +331,32 @@ func createErrorInvalidRelease(hint string) error {
 	return errors.New(errMsg)
 }
 
+func createErrorProfileDNotFound() error {
+	const profileD = "/etc/profile.d"
+
+	var builder strings.Builder
+	fmt.Fprintf(&builder, "directory %s not found in container\n", profileD)
+	fmt.Fprintf(&builder, "The shell start-up scripts must include files from %s in\n", profileD)
+	fmt.Fprintf(&builder, "containers.\n")
+	fmt.Fprintf(&builder, "Go to https://containertoolbx.org/ for further information.")
+
+	errMsg := builder.String()
+	return errors.New(errMsg)
+}
+
+func createErrorSudoersDNotFound() error {
+	const sudoersD = "/etc/sudoers.d"
+
+	var builder strings.Builder
+	fmt.Fprintf(&builder, "directory %s not found in container\n", sudoersD)
+	fmt.Fprintf(&builder, "The sudoers(5) policy must include files from %s in\n", sudoersD)
+	fmt.Fprintf(&builder, "containers with /etc/pkcs11/modules and p11-kit-client.so.\n")
+	fmt.Fprintf(&builder, "Go to https://containertoolbx.org/ for further information.")
+
+	errMsg := builder.String()
+	return errors.New(errMsg)
+}
+
 func getCDIFileForNvidia(targetUser *user.User) (string, error) {
 	toolboxRuntimeDirectory, err := utils.GetRuntimeDirectory(targetUser)
 	if err != nil {