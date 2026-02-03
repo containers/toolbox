81a9d96c13091f8f67ca6fcb9ff43ee09aa03f1a
Debarshi Ray <rishi@fedoraproject.org>
2026-02-03 22:52:47 +0100

    Update copyright notices
    https://github.com/containers/toolbox/pull/1758

--- test/system/setup_suite.bash
+++ test/system/setup_suite.bash
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 #
-# Copyright © 2021 – 2025 Red Hat, Inc.
+# Copyright © 2021 – 2026 Red Hat, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
