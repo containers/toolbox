containers/toolbox

commit 56fcb0b4d26adb9c0de7856e4aad6acc37411e9d

cmd/list, pkg/podman: Don't unmarshal the 'podman ps' JSON twice
This builds on top of commit e7722078310a79b0.

Currently, the JSON from 'podman ps --format json' gets unmarshalled
into a []map[string]interface{} in podman.GetContainers, where the maps
in the slice represent containers.  Each map is then marshalled back
into JSON and then again unmarshalled into a toolboxContainer type.

This is wasteful.  The toolboxContainer type already implements the
json.Unmarshaler interface [1], since commit e7722078310a79b0.  Hence,
the entire JSON from 'podman ps --format json' can be directly
unmarshalled into a slice of toolboxContainers without involving the
[]map[string]interface{}.

A subsequent commit will move the toolboxContainer type into the podman
package to more tightly encapsulate the unmarshalling of the JSON.  So,
as an intermediate step in that direction, the podman.GetContainers
function has been temporarily changed to return the entire JSON.

[1] https://pkg.go.dev/encoding/json#Unmarshaler

https://github.com/containers/toolbox/pull/1485

src/pkg/podman/podman.go
@@ -155,14 +155,14 @@ func ContainerExists(container string) (bool, error) {
 	return true, nil
 }
 
-// GetContainers is a wrapper function around `podman ps --format json` command.
+// GetContainersJSON is a wrapper function around `podman ps --format json` command.
 //
 // Parameter args accepts an array of strings to be passed to the wrapped command (eg. ["-a", "--filter", "123"]).
 //
-// Returned value is a slice of dynamically unmarshalled json, so it needs to be treated properly.
+// Returned value is the JSON representing the containers.
 //
 // If a problem happens during execution, first argument is nil and second argument holds the error message.
-func GetContainers(args ...string) ([]map[string]interface{}, error) {
+func GetContainersJSON(args ...string) ([]byte, error) {
 	var stdout bytes.Buffer
 
 	logLevelString := LogLevel.String()
@@ -172,14 +172,8 @@ func GetContainers(args ...string) ([]map[string]interface{}, error) {
 		return nil, err
 	}
 
-	output := stdout.Bytes()
-	var containers []map[string]interface{}
-
-	if err := json.Unmarshal(output, &containers); err != nil {
-		return nil, err
-	}
-
-	return containers, nil
+	data := stdout.Bytes()
+	return data, nil
 }
 
 // GetImages is a wrapper function around `podman images --format json` command.

src/cmd/list.go
@@ -126,8 +126,14 @@ func list(cmd *cobra.Command, args []string) error {
 func getContainers() ([]toolboxContainer, error) {
 	logrus.Debug("Fetching all containers")
 	args := []string{"--all", "--sort", "names"}
-	containers, err := podman.GetContainers(args...)
+	data, err := podman.GetContainersJSON(args...)
 	if err != nil {
+		logrus.Debugf("Fetching all containers failed: %s", err)
+		return nil, errors.New("failed to get containers")
+	}
+
+	var containers []toolboxContainer
+	if err := json.Unmarshal(data, &containers); err != nil {
 		logrus.Debugf("Fetching all containers failed: %s", err)
 		return nil, errors.New("failed to get containers")
 	}
@@ -135,23 +141,9 @@ func getContainers() ([]toolboxContainer, error) {
 	var toolboxContainers []toolboxContainer
 
 	for _, container := range containers {
-		var c toolboxContainer
-
-		containerJSON, err := json.Marshal(container)
-		if err != nil {
-			logrus.Errorf("failed to marshal container: %v", err)
-			continue
-		}
-
-		err = c.UnmarshalJSON(containerJSON)
-		if err != nil {
-			logrus.Errorf("failed to unmarshal container: %v", err)
-			continue
-		}
-
 		for label := range toolboxLabels {
-			if _, ok := c.Labels[label]; ok {
-				toolboxContainers = append(toolboxContainers, c)
+			if _, ok := container.Labels[label]; ok {
+				toolboxContainers = append(toolboxContainers, container)
 				break
 			}
 		}

commit d85415e66ef789a61bdd020d19a4236ea995160d

pkg/podman: Rename variables for consistency
Fallout from 56fcb0b4d26adb9c0de7856e4aad6acc37411e9d and
2486e25601331a9305ed559cb86ed807b4ea0836

https://github.com/containers/toolbox/pull/1768

src/pkg/podman/podman.go
@@ -179,9 +179,9 @@ func GetContainers(args ...string) (*Containers, error) {
 		return nil, err
 	}
 
-	data := stdout.Bytes()
+	output := stdout.Bytes()
 	var containers []containerPS
-	if err := json.Unmarshal(data, &containers); err != nil {
+	if err := json.Unmarshal(output, &containers); err != nil {
 		return nil, err
 	}
 
@@ -204,9 +204,9 @@ func GetImages(args ...string) ([]Image, error) {
 		return nil, err
 	}
 
-	data := stdout.Bytes()
+	output := stdout.Bytes()
 	var images []Image
-	if err := json.Unmarshal(data, &images); err != nil {
+	if err := json.Unmarshal(output, &images); err != nil {
 		return nil, err
 	}
 